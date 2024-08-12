22df60aceaae90e62309b7c699dc8b29088f5400
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-08-11 20:12:28 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-		"./index.html",
-		"./src/**/*.{js,ts,jsx,tsx}", // Include all your JS/TS and JSX/TSX files
-	],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
 	},
