03b09c0f6d073e64826d730b320e22a92bad32b4
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-08-16 17:13:24 -0700

    Created Private Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,15 +11,18 @@ import { RootLayout } from './layouts/RootLayout';
 import { Home } from './pages/Home';
 import { PartyMode } from './pages/PartyMode';
 import { PartyModeLayout } from './layouts/PartyModeLayout';
+import PrivateRoutes from './utils/PrivateRoutes';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<Home />}></Route>
+    <Route element={<PrivateRoutes />}>
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />}></Route>
 
-      <Route path="party-mode" element={<PartyModeLayout />}>
-        <Route index element={<Navigate to="party-mode-main" replace />}></Route>
-        <Route path="party-mode-main" element={<PartyMode />}></Route>
+        <Route path="party-mode" element={<PartyModeLayout />}>
+          <Route index element={<Navigate to="party-mode-main" replace />}></Route>
+          <Route path="party-mode-main" element={<PartyMode />}></Route>
+        </Route>
       </Route>
     </Route>,
   ),
