b86a7d2da452d5d41db58a19bccd6af4acf6180e
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-08-17 13:00:07 -0700

    Update UserPlaylist.tsx

--- frontend/src/components/UserPlaylist.tsx
+++ frontend/src/components/UserPlaylist.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { CurrentUserPlaylistImage } from '../types/playlists/currentUserPlaylist';
 
 type UserPlaylistProps = {
@@ -17,14 +18,16 @@ export const UserPlaylist = ({ name, images, owner, type }: UserPlaylistProps) =
   }
 
   return (
-    <div className="flex w-full cursor-pointer items-center gap-2 py-2 hover:bg-blue-50">
-      <img className="h-20 w-20" src={image} />
-      <div className="flex flex-col">
-        <p>{name}</p>
-        <p>
-          {type} + {owner}
-        </p>
+    <Link to={'/party-mode/search'}>
+      <div className="flex w-full items-center gap-2 py-2 hover:bg-blue-50">
+        <img className="h-20 w-20" src={image} />
+        <div className="flex flex-col">
+          <p>{name}</p>
+          <p>
+            {type} + {owner}
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
