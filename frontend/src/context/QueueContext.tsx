76c9694b807f0f68fa6a2dff781a68f9fa8fa931
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-08-24 11:10:08 -0700

    Create QueueContext.tsx

---
+++ frontend/src/context/QueueContext.tsx
@@ -0,0 +1,29 @@
+import { createContext, ReactNode, useState } from 'react';
+
+type QueueProviderProps = {
+  children: ReactNode;
+};
+
+type QueueContext = {
+  priorityQueue: SpotifyApi.TrackObjectFull[];
+  //prettier-ignore
+  setPriorityQueue: React.Dispatch<React.SetStateAction<SpotifyApi.TrackObjectFull[]>>;
+  playlistQueue: SpotifyApi.TrackObjectFull[];
+  //prettier-ignore
+  setPlaylistQueue: React.Dispatch<React.SetStateAction<SpotifyApi.TrackObjectFull[]>>;
+};
+
+const QueueContext = createContext<QueueContext | undefined>(undefined);
+
+export const QueueProvider = ({ children }: QueueProviderProps) => {
+  const [priorityQueue, setPriorityQueue] = useState<SpotifyApi.TrackObjectFull[]>([]);
+  const [playlistQueue, setPlaylistQueue] = useState<SpotifyApi.TrackObjectFull[]>([]);
+  return (
+    <QueueContext.Provider
+      value={{ priorityQueue, setPriorityQueue, setPlaylistQueue, playlistQueue }}
+    >
+      {children}
+    </QueueContext.Provider>
+  );
+};
+export { QueueContext };
