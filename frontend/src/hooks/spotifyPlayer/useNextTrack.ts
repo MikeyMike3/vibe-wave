35be4fbb7a9248940641311f9b1c6655cb6b9442
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-08-25 15:33:54 -0700

    Update useNextTrack.ts

--- frontend/src/hooks/spotifyPlayer/useNextTrack.ts
+++ frontend/src/hooks/spotifyPlayer/useNextTrack.ts
@@ -14,6 +14,7 @@ export const useNextTrack = () => {
       playSong(player, deviceId, priorityQueue[0].uri);
     } else if (playlistQueue.length > 0) {
       // the index logic is handled within the SpotifyPlayer Component in the 'player_state_changed' listener
+      // the index gets 1 added to it whenever the song changes
       playSong(player, deviceId, playlistQueue[playlistQueueIndexRef.current]?.track?.uri);
     }
   };
