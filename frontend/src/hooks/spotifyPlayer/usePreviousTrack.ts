40faa21c53c62ee578666eb17c7d2de3c0338077
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-08-25 16:04:17 -0700

    Update usePreviousTrack.ts

--- frontend/src/hooks/spotifyPlayer/usePreviousTrack.ts
+++ frontend/src/hooks/spotifyPlayer/usePreviousTrack.ts
@@ -6,7 +6,7 @@ export const usePreviousTrack = () => {
   const playSong = usePlaySong();
   const previousTrack = () => {
     if (playlistQueue.length > 0 && playlistQueueIndexRef.current > 1) {
-      // playlistQueueIndexRef.current must be subtracted by 2
+      // playlistQueueIndexRef.current must be subtracted by 2 to be able to play the previous song
       playlistQueueIndexRef.current -= 2;
       console.log(playlistQueueIndexRef.current);
       playSong(playlistQueue[playlistQueueIndexRef.current].track?.uri);
