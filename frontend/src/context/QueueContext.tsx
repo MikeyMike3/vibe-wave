5ebc2bdc979b14b6b3abb56e532c11dce54531d8
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-08-25 14:33:20 -0700

    Update QueueContext.tsx

--- frontend/src/context/QueueContext.tsx
+++ frontend/src/context/QueueContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useRef, useState } from 'react';
 
 type QueueProviderProps = {
   children: ReactNode;
@@ -11,17 +11,27 @@ type QueueContext = {
   playlistQueue: SpotifyApi.PlaylistTrackObject[];
   //prettier-ignore
   setPlaylistQueue: React.Dispatch<React.SetStateAction<SpotifyApi.PlaylistTrackObject[]>>;
+  //prettier-ignore
+  playlistQueueIndexRef: React.MutableRefObject<number>;
 };
 
 const QueueContext = createContext<QueueContext | undefined>(undefined);
 
 export const QueueProvider = ({ children }: QueueProviderProps) => {
   const [priorityQueue, setPriorityQueue] = useState<SpotifyApi.TrackObjectFull[]>([]);
-  const [playlistQueue, setPlaylistQueue] = useState<SpotifyApi.TrackObjectFull[]>([]);
+  const [playlistQueue, setPlaylistQueue] = useState<SpotifyApi.PlaylistTrackObject[]>([]);
+
+  const playlistQueueIndexRef = useRef(0);
 
   return (
     <QueueContext.Provider
-      value={{ priorityQueue, setPriorityQueue, setPlaylistQueue, playlistQueue }}
+      value={{
+        priorityQueue,
+        setPriorityQueue,
+        setPlaylistQueue,
+        playlistQueue,
+        playlistQueueIndexRef,
+      }}
     >
       {children}
     </QueueContext.Provider>
