d44aa4fd19c1e6013ea93104a885319d6e0ff2fa
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-08-24 22:39:35 -0700

    Update PCNav.tsx

--- frontend/src/components/nav/PCNav.tsx
+++ frontend/src/components/nav/PCNav.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export const PCNav = () => {
   return (
-    <div className="mx-auto max-w-[95%] min-[1500px]:w-[1800px]">
+    <div className="mx-auto w-[95%] min-[1500px]:w-[1800px]">
       <div className="flex justify-between text-xl">
         <div>
           <Link to={'/'}>VibeWave</Link>
