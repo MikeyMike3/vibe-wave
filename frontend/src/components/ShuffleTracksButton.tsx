98d1addefaf2c59e6ab2b06583ec06dc33dffc66
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-08-27 15:18:28 -0700

    Update ShuffleTracksButton.tsx

--- frontend/src/components/ShuffleTracksButton.tsx
+++ frontend/src/components/ShuffleTracksButton.tsx
@@ -1,7 +1,14 @@
-
+import { useState } from 'react';
+import { usePlaybackContext } from '../hooks/context/usePlaybackContext';
 
 export const ShuffleTracksButton = () => {
-  return (
-    <div>Shuffle</div>
-  )
-}
+  const { shuffleTracksRef } = usePlaybackContext();
+  const [shuffleTracks, setShuffleTracks] = useState(false);
+
+  const handleClick = () => {
+    shuffleTracksRef.current = !shuffleTracksRef.current;
+    setShuffleTracks(shuffle => !shuffle);
+  };
+
+  return <button onClick={handleClick}>Shuffle</button>;
+};
