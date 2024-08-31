2452e4f8db2ba83e8ef0af53da3f0a3794cdd1ab
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-08-31 13:30:34 -0700

    Create useIndexPlaylistQueue.ts

---
+++ frontend/src/hooks/spotifyPlayer/useIndexPlaylistQueue.ts
@@ -0,0 +1,19 @@
+import { useQueueContext } from '../context/useQueueContext';
+
+export const useIndexPlaylistQueue = () => {
+  const { setPlaylistQueueIndex, playlistQueueIndexRef } = useQueueContext();
+  const indexPlaylistQueue = (indexTo: number, arithmetic?: '+' | '-' | 'set') => {
+    if (arithmetic === 'set') {
+      playlistQueueIndexRef.current = indexTo;
+      setPlaylistQueueIndex(playlistQueueIndexRef.current);
+    } else if (arithmetic === '+') {
+      playlistQueueIndexRef.current += indexTo;
+      setPlaylistQueueIndex(playlistQueueIndexRef.current);
+    } else if (arithmetic === '-') {
+      playlistQueueIndexRef.current -= indexTo;
+      setPlaylistQueueIndex(playlistQueueIndexRef.current);
+    }
+  };
+
+  return indexPlaylistQueue;
+};
