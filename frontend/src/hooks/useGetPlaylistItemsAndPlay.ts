23421ccf25b1dfac7432989750782430c5c6ccb9
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-09-01 11:34:12 -0700

    Update useGetPlaylistItemsAndPlay.ts

--- frontend/src/hooks/useGetPlaylistItemsAndPlay.ts
+++ frontend/src/hooks/useGetPlaylistItemsAndPlay.ts
@@ -3,30 +3,27 @@ import { usePlaybackContext } from './context/usePlaybackContext';
 import { useQueueContext } from './context/useQueueContext';
 import { usePlaySong } from './spotifyPlayer/usePlaySong';
 import { useIndexPlaylistQueue } from './spotifyPlayer/useIndexPlaylistQueue';
-import { useSpotifyPlayerContext } from './context/useSpotifyPlayerContext';
 
 export const useGetPlaylistItemsAndPlay = (playlistId: string, playlistName: string) => {
   const { setPlaylistName } = usePlaybackContext();
   const { setPlaylistQueue, unShuffledQueueRef } = useQueueContext();
-  const { player } = useSpotifyPlayerContext();
+
   const indexPlaylistQueue = useIndexPlaylistQueue();
   const getPlaylistItems = useGetPlaylistItems(playlistId);
   const playSong = usePlaySong();
 
   const getPlaylistItemsAndPlay = async () => {
-    if (player) {
-      const data = await getPlaylistItems();
-      setPlaylistQueue(data.items);
-      setPlaylistName(playlistName);
-      setPlaylistQueue(currentQueue => {
-        if (currentQueue.length > 0) {
-          playSong(currentQueue[0]?.track?.uri);
-          unShuffledQueueRef.current = currentQueue;
-          indexPlaylistQueue(0, 'set');
-        }
-        return currentQueue;
-      });
-    }
+    const data = await getPlaylistItems();
+    setPlaylistQueue(data.items);
+    setPlaylistName(playlistName);
+    setPlaylistQueue(currentQueue => {
+      if (currentQueue.length > 0) {
+        playSong(currentQueue[0]?.track?.uri);
+        unShuffledQueueRef.current = currentQueue;
+        indexPlaylistQueue(0, 'set');
+      }
+      return currentQueue;
+    });
   };
 
   return getPlaylistItemsAndPlay;
