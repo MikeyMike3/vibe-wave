1ad7feecfc79f1f828277b4c8b55b6869968c847
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-09-01 14:32:20 -0700

    Update useGetPlaylistItemsAndPlay.ts

--- frontend/src/hooks/useGetPlaylistItemsAndPlay.ts
+++ frontend/src/hooks/useGetPlaylistItemsAndPlay.ts
@@ -2,12 +2,14 @@ import { useGetPlaylistItems } from './apis/useGetPlaylistItems';
 import { usePlaybackContext } from './context/usePlaybackContext';
 import { useQueueContext } from './context/useQueueContext';
 import { useIndexPlaylistQueue } from './spotifyPlayer/useIndexPlaylistQueue';
+import { usePlaySong } from './spotifyPlayer/usePlaySong';
 import { useShuffleTracks } from './spotifyPlayer/useShuffleTracks';
 
 export const useGetPlaylistItemsAndPlay = (playlistId: string, playlistName: string) => {
-  const { setPlaylistName } = usePlaybackContext();
+  const { setPlaylistName, shuffleTracksRef } = usePlaybackContext();
   const { setPlaylistQueue, unShuffledQueueRef } = useQueueContext();
   const shuffleTracks = useShuffleTracks();
+  const playSong = usePlaySong();
 
   const indexPlaylistQueue = useIndexPlaylistQueue();
   const getPlaylistItems = useGetPlaylistItems(playlistId);
@@ -23,6 +25,9 @@ export const useGetPlaylistItemsAndPlay = (playlistId: string, playlistName: str
         indexPlaylistQueue(0, 'set');
         unShuffledQueueRef.current = currentQueue;
         shuffleTracks({ prevQueue: [...currentQueue] });
+        if (!shuffleTracksRef.current) {
+          playSong(currentQueue[0].track?.uri);
+        }
       }
       return currentQueue;
     });
