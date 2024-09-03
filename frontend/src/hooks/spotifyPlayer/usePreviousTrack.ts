a7b0d0fb92bb6d6a75075e14c627f13898c0990a
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-09-02 17:09:50 -0700

    Update usePreviousTrack.ts
    The song now goes back to the start when the user clicks prev track if the song position is greater than 5000 milliseconds

--- frontend/src/hooks/spotifyPlayer/usePreviousTrack.ts
+++ frontend/src/hooks/spotifyPlayer/usePreviousTrack.ts
@@ -2,14 +2,21 @@ import { usePlaySong } from './usePlaySong';
 import { useQueueContext } from '../context/useQueueContext';
 import { usePlaybackContext } from '../context/usePlaybackContext';
 import { useIndexPlaylistQueue } from './useIndexPlaylistQueue';
+import { useSpotifyPlayerContext } from '../context/useSpotifyPlayerContext';
 
 export const usePreviousTrack = () => {
   const { playlistQueue, playlistQueueIndexRef } = useQueueContext();
-  const { repeatRef, setRepeat, userPreviousTrackRef } = usePlaybackContext();
+  const { repeatRef, setRepeat, userPreviousTrackRef, playerPosition } = usePlaybackContext();
+  const { player } = useSpotifyPlayerContext();
   const playSong = usePlaySong();
   const indexPlaylistQueue = useIndexPlaylistQueue();
   const previousTrack = () => {
     userPreviousTrackRef.current = true;
+
+    if (Number(playerPosition) > 5000) {
+      player?.seek(0);
+      return;
+    }
 
     if (repeatRef.current === 2) {
       repeatRef.current = 1;
