41fbb8e3612898bda027fefad59b9b45ba282cf0
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-09-04 16:27:31 -0700

    Update PlaySkipButton.tsx

--- frontend/src/components/spotifyPlayer/PlaySkipButton.tsx
+++ frontend/src/components/spotifyPlayer/PlaySkipButton.tsx
@@ -1,7 +1,10 @@
+import { usePlaySkip } from '../../hooks/spotifyPlayer/usePlaySkip';
+
 type PlaySkipButtonProps = {
   name: string;
 };
 
 export const PlaySkipButton = ({ name }: PlaySkipButtonProps) => {
-  return <button>PlaySkip</button>;
+  const playSkip = usePlaySkip();
+  return <button onClick={() => playSkip(name)}>PlaySkip</button>;
 };
