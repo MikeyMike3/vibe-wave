23ccf7a5cd1c461a0f13e4afd23030bfa7f7a8f7
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-09-04 16:26:59 -0700

    Create usePlaySkip.tsx

---
+++ frontend/src/hooks/spotifyPlayer/usePlaySkip.tsx
@@ -0,0 +1,16 @@
+import { useQueueContext } from '../context/useQueueContext';
+import { useIndexPlaylistQueue } from './useIndexPlaylistQueue';
+import { usePlaySong } from './usePlaySong';
+
+export const usePlaySkip = () => {
+  const { playlistQueue, playlistQueueIndexRef } = useQueueContext();
+  const playSong = usePlaySong();
+  const indexPlaylistQueue = useIndexPlaylistQueue();
+
+  const playSkip = (name: string) => {
+    const index = playlistQueue.findIndex(item => item.track?.name === name);
+    indexPlaylistQueue(index, 'set');
+    playSong(playlistQueue[playlistQueueIndexRef.current].track?.uri);
+  };
+  return playSkip;
+};
