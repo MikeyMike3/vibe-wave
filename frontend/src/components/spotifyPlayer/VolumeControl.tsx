fbd13826404c02533d1a7697efff26f758e39b2d
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-09-05 19:19:09 -0700

    Added Additional Functionality to VolumeControls
    You can now click the volume button and it will mute the song. Clicking the volume again sets the volume back to the previous value.

--- frontend/src/components/spotifyPlayer/VolumeControl.tsx
+++ frontend/src/components/spotifyPlayer/VolumeControl.tsx
@@ -1,12 +1,15 @@
 import { useSpotifyPlayerContext } from '../../hooks/context/useSpotifyPlayerContext';
 import { usePlaybackContext } from '../../hooks/context/usePlaybackContext';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 export const VolumeControl = () => {
-  const { player } = useSpotifyPlayerContext();
+  const { player, isPlayerReady } = useSpotifyPlayerContext();
   const { volume, setVolume } = usePlaybackContext();
 
   const volumeRef = useRef<HTMLInputElement>(null);
+  const prevVolumeRef = useRef(0.1);
+  const isVolumeIconClickedRef = useRef(false);
+  const hasUserInteractedRef = useRef(false);
 
   const updateSliderBackground = (value: number | string) => {
     const percentage = Number(value);
@@ -21,9 +24,54 @@ export const VolumeControl = () => {
     setVolume(value)
     player?.setVolume(value / 100)
     updateSliderBackground(value)
+    prevVolumeRef.current = value;
   }
 
+  const handleClick = () => {
+    if (!hasUserInteractedRef.current) {
+      prevVolumeRef.current = 10;
+      setVolume(0);
+      player?.setVolume(0);
+      updateSliderBackground(0);
+      hasUserInteractedRef.current = true;
+      return;
+    }
+    if (isVolumeIconClickedRef.current) {
+      setVolume(0);
+      player?.setVolume(0);
+      updateSliderBackground(0);
+      isVolumeIconClickedRef.current = false;
+    } else {
+      setVolume(prevVolumeRef.current);
+      player?.setVolume(prevVolumeRef.current / 100);
+      updateSliderBackground(prevVolumeRef.current);
+      isVolumeIconClickedRef.current = true;
+    }
+  };
+
+  useEffect(() => {
+    updateSliderBackground(10);
+    const getVolumeFunc = async () => {
+      if (isPlayerReady && player?.getVolume) {
+        const getVolume = (await player.getVolume()) * 100;
+        setVolume(getVolume);
+        updateSliderBackground(getVolume);
+      }
+    };
+    getVolumeFunc();
+  }, [player?.getVolume, player, setVolume, isPlayerReady]);
+
   return (
-    <input ref={volumeRef} type="range" min="0" max="100" onChange={handleChange} value={volume} />
+    <div className="flex items-center gap-2">
+      <button onClick={handleClick}>Volume</button>
+      <input
+        ref={volumeRef}
+        type="range"
+        min="0"
+        max="100"
+        onChange={handleChange}
+        value={volume}
+      />
+    </div>
   );
 };
