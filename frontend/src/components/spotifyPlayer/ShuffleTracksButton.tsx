b0fbe02bcda4f3acedf6ddb13a25202f69df1dca
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-09-09 17:28:48 -0700

    Update ShuffleTracksButton.tsx

--- frontend/src/components/spotifyPlayer/ShuffleTracksButton.tsx
+++ frontend/src/components/spotifyPlayer/ShuffleTracksButton.tsx
@@ -1,5 +1,7 @@
 import { usePlaybackContext } from '../../hooks/context/usePlaybackContext';
 import { useShuffleTracks } from '../../hooks/spotifyPlayer/useShuffleTracks';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShuffle } from '@fortawesome/free-solid-svg-icons';
 
 export const ShuffleTracksButton = () => {
   const { shuffleTracksState } = usePlaybackContext();
@@ -8,9 +10,16 @@ export const ShuffleTracksButton = () => {
   return (
     <>
       {shuffleTracksState ? (
-        <button onClick={() => shuffleTracks({ shouldChangeState: true })}>Shuffled</button>
+        <button
+          className="text- duration-150 hover:text-textPrimary"
+          onClick={() => shuffleTracks({ shouldChangeState: true })}
+        >
+          <FontAwesomeIcon className="text-2xl" icon={faShuffle} />
+        </button>
       ) : (
-        <button onClick={() => shuffleTracks({ shouldChangeState: true })}>Shuffle</button>
+        <button onClick={() => shuffleTracks({ shouldChangeState: true })}>
+          <FontAwesomeIcon icon={faShuffle} style={{ fontSize: '1.5rem', color: 'gray' }} />
+        </button>
       )}
     </>
   );
