5ee1964b554530ed4571fa215f7b018081f6c4cd
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-09-09 17:21:49 -0700

    Update PreviousTrackButton.tsx

--- frontend/src/components/spotifyPlayer/PreviousTrackButton.tsx
+++ frontend/src/components/spotifyPlayer/PreviousTrackButton.tsx
@@ -1,7 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBackwardStep } from '@fortawesome/free-solid-svg-icons';
 import { usePreviousTrack } from '../../hooks/spotifyPlayer/usePreviousTrack';
 
 export const PreviousTrackButton = () => {
   const previousTrack = usePreviousTrack();
 
-  return <button onClick={previousTrack}>Previous</button>;
+  return (
+    <button className="text-textAccent duration-150 hover:text-textPrimary" onClick={previousTrack}>
+      <FontAwesomeIcon className="text-2xl" icon={faBackwardStep} />
+    </button>
+  );
 };
