ebac6cc34d03754bd2e7f3493e7f88ac654e0138
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-09-13 15:53:36 -0700

    Update SearchResultTrackItem.tsx

--- frontend/src/components/SearchResultTrackItem.tsx
+++ frontend/src/components/SearchResultTrackItem.tsx
@@ -1,24 +1,16 @@
-import { getImageUrl } from '../functions/getImageUrl';
 import { AddToFrontOfPriorityQueueButton } from './AddToFrontOfPriorityQueueButton';
 import { AddToQueueButton } from './AddToQueueButton';
 import PlaySongButton from './PlaySongButton';
+import { TrackInfo } from './TrackInfo';
 
 type SearchResultTrackItemProps = {
   track: SpotifyApi.TrackObjectFull;
 };
 
 export const SearchResultTrackItem = ({ track }: SearchResultTrackItemProps) => {
-  const image = getImageUrl(track.album.images);
-
   return (
-    <div className="flex w-full items-center justify-between py-2 hover:bg-blue-50">
-      <div className="flex items-center gap-2">
-        <img className="h-20 w-20" src={image} />
-        <div className="flex flex-col">
-          <p>{track.name}</p>
-          <p>{track.artists.map(item => item.name).join(', ')}</p>
-        </div>
-      </div>
+    <div className="flex w-full items-center justify-between p-2 py-2 hover:bg-bgAccent">
+      <TrackInfo images={track.album.images} name={track.name} artists={track.artists} />
       <div className="flex gap-3">
         <AddToQueueButton track={track} />
         <AddToFrontOfPriorityQueueButton track={track} />
