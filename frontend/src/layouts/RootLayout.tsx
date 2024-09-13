5394633c36d4413c1884cffae18ab3ea2c3a51ac
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-09-12 17:50:11 -0700

    Refactoring

--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -2,7 +2,7 @@ import { PCNav } from '../components/nav/PCNav';
 import { MobileNav } from '../components/nav/MobileNav';
 import { Outlet } from 'react-router-dom';
 import { SpotifyPlayer } from '../components/spotifyPlayer/SpotifyPlayer';
-import { Search } from '../pages/Search';
+import { SearchBar } from '../components/SearchBar';
 
 export const RootLayout = () => {
   return (
@@ -13,10 +13,8 @@ export const RootLayout = () => {
           <MobileNav />
         </div>
         <div className="overflow-y-scroll pb-32">
-          <Search />
-          <div className="pt-7">
-            <Outlet />
-          </div>
+          <SearchBar />
+          <Outlet />
         </div>
       </div>
 
