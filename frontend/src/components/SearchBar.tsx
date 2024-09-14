d0ce677f212d9ce2dfd984a33f9a319ee2977a18
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-09-13 17:12:38 -0700

    Fixed Bug within SearchBar.tsx
    When the use would click and drag text within the input it would instead drag the Link

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useSearchContext } from '../hooks/context/useSearchContext';
 import { useFetchSearchResults } from '../hooks/apis/useFetchSearchResults';
 
@@ -7,6 +7,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@awesome.me/kit-71c07605c0/icons/sharp/solid';
 
 export const SearchBar = () => {
+  const navigate = useNavigate();
+
   const { setSearchResults, query, setQuery } = useSearchContext();
   const fetchSearchResults = useFetchSearchResults();
 
@@ -18,18 +20,18 @@ export const SearchBar = () => {
       fetchSearchResults(query);
     }
   };
-  // prettier-ignore
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      if (inputRef.current?.value) {
-        inputRef.current.value = e.target.value;
-        setQuery(inputRef.current.value)
-      }
-  
-      if (inputRef.current?.value.length === 0) {
-        setSearchResults({})
-      }
-      
-    };
+
+  //prettier-ignore
+  const handleChange = (e:  React.ChangeEvent<HTMLInputElement>) => {
+    if (inputRef.current?.value) {
+      inputRef.current.value = e.target.value;
+      setQuery(inputRef.current.value);
+    }
+
+    if (inputRef.current?.value.length === 0) {
+      setSearchResults({});
+    }
+  };
 
   useEffect(() => {
     if (query.length > 0) {
@@ -39,23 +41,25 @@ export const SearchBar = () => {
 
   return (
     <div className="sticky top-0 z-[9999] flex w-full justify-center bg-black">
-      <Link to={'/search'}>
-        <form onSubmit={handleSubmit} className="flex items-center py-5">
-          <div className="flex items-center gap-2 rounded-full border-2 border-transparent bg-bgAccent px-6 py-2 transition duration-150 focus-within:border-magenta">
-            <FontAwesomeIcon
-              onSubmit={handleSubmit}
-              className="text-3xl text-textAccent duration-150 hover:text-textPrimary"
-              icon={faMagnifyingGlass}
-            />
-            <input
-              className="w-[500px] rounded-xl bg-bgAccent p-2 text-white outline-none"
-              ref={inputRef}
-              onChange={handleChange}
-              placeholder="Search"
-            />
-          </div>
-        </form>
-      </Link>
+      <form
+        onClick={() => navigate('/search')}
+        onSubmit={handleSubmit}
+        className="flex items-center py-5"
+      >
+        <div className="flex items-center gap-2 rounded-full border-2 border-transparent bg-bgAccent px-6 py-2 transition duration-150 focus-within:border-magenta">
+          <FontAwesomeIcon
+            onSubmit={handleSubmit}
+            className="text-3xl text-textAccent duration-150 hover:text-textPrimary"
+            icon={faMagnifyingGlass}
+          />
+          <input
+            className="w-[500px] rounded-xl bg-bgAccent p-2 text-white outline-none"
+            ref={inputRef}
+            onChange={handleChange}
+            placeholder="Search"
+          />
+        </div>
+      </form>
     </div>
   );
 };
