0eb9000ff433fdd39c8d4f36f0ba3bfb4c98dbf9
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-09-14 17:53:37 -0700

    Update SearchResultArtistItem.tsx

--- frontend/src/components/SearchResultArtistItem.tsx
+++ frontend/src/components/SearchResultArtistItem.tsx
@@ -13,7 +13,7 @@ export const SearchResultArtistItem = ({ artist }: SearchResultArtistItemProps) 
       to={'/artist'}
       className="w-64 flex-shrink-0 rounded-xl py-4 duration-150 hover:bg-bgAccent"
     >
-      <img className="mx-auto h-64 w-full rounded-full" src={image} />
+      <img className="mx-auto h-64 w-full rounded-full px-2" src={image} />
       <p className="pt-3 text-center text-textPrimary">{artist.name}</p>
     </Link>
   );
