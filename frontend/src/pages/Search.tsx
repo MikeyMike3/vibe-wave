f34a53d0809b5a00b06d5a879830be0f1fd6f191
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-09-14 18:41:56 -0700

    Update Search.tsx

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -14,87 +14,106 @@ export const Search = () => {
 
   return (
     <>
-      <div className="flex gap-4 py-4 text-white">
-        <button
-          className={`${isAllClicked && 'bg-white text-black'} rounded-xl bg-bgAccent p-2 px-4`}
-          onClick={() => {
-            setIsAllClicked(true);
-            setIsTracksClicked(false);
-            setIsArtistsClicked(false);
-            setIsAlbumsClicked(false);
-          }}
-        >
-          All
-        </button>
-        <button
-          className={`${isTracksClicked && 'bg-white text-black'} rounded-xl bg-bgAccent p-2 px-4`}
-          onClick={() => {
-            setIsAllClicked(false);
-            setIsTracksClicked(true);
-            setIsArtistsClicked(false);
-            setIsAlbumsClicked(false);
-          }}
-        >
-          Tracks
-        </button>
-        <button
-          className={`${isArtistsClicked && 'bg-white text-black'} rounded-xl bg-bgAccent p-2 px-4`}
-          onClick={() => {
-            setIsAllClicked(false);
-            setIsTracksClicked(false);
-            setIsArtistsClicked(true);
-            setIsAlbumsClicked(false);
-          }}
-        >
-          Artists
-        </button>
-        <button
-          className={`${isAlbumsClicked && 'bg-white text-black'} rounded-xl bg-bgAccent p-2 px-4`}
-          onClick={() => {
-            setIsAllClicked(false);
-            setIsTracksClicked(false);
-            setIsArtistsClicked(false);
-            setIsAlbumsClicked(true);
-          }}
-        >
-          Albums
-        </button>
-      </div>
-
-      {isAllClicked && (
-        <AllSearchResults
-          albums={searchResults.albums}
-          artists={searchResults.artists}
-          tracks={searchResults.tracks}
-        />
-      )}
-
-      {isTracksClicked && (
+      {searchResults.albums && searchResults.artists && searchResults.tracks && (
         <>
-          <h2 className="pb-4 text-3xl text-textPrimary">Tracks</h2>
-          {searchResults?.tracks?.items.map(item => (
-            <SearchResultTrackItem key={item.id} track={item} />
-          ))}
-        </>
-      )}
-      {isArtistsClicked && (
-        <>
-          <h2 className="pb-4 text-3xl text-textPrimary">Artists</h2>
-          <div className="grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-7">
-            {searchResults?.artists?.items.map(item => (
-              <SearchResultArtistItem key={item.id} artist={item} />
-            ))}
+          <div className="flex gap-4 py-4 text-white">
+            <button
+              className={`${isAllClicked && 'bg-white text-black'} rounded-xl bg-bgAccent p-2 px-4`}
+              onClick={() => {
+                setIsAllClicked(true);
+                setIsTracksClicked(false);
+                setIsArtistsClicked(false);
+                setIsAlbumsClicked(false);
+              }}
+            >
+              All
+            </button>
+            <button
+              className={`${isTracksClicked && 'bg-white text-black'} rounded-xl bg-bgAccent p-2 px-4`}
+              onClick={() => {
+                setIsAllClicked(false);
+                setIsTracksClicked(true);
+                setIsArtistsClicked(false);
+                setIsAlbumsClicked(false);
+              }}
+            >
+              Tracks
+            </button>
+            <button
+              className={`${isArtistsClicked && 'bg-white text-black'} rounded-xl bg-bgAccent p-2 px-4`}
+              onClick={() => {
+                setIsAllClicked(false);
+                setIsTracksClicked(false);
+                setIsArtistsClicked(true);
+                setIsAlbumsClicked(false);
+              }}
+            >
+              Artists
+            </button>
+            <button
+              className={`${isAlbumsClicked && 'bg-white text-black'} rounded-xl bg-bgAccent p-2 px-4`}
+              onClick={() => {
+                setIsAllClicked(false);
+                setIsTracksClicked(false);
+                setIsArtistsClicked(false);
+                setIsAlbumsClicked(true);
+              }}
+            >
+              Albums
+            </button>
           </div>
-        </>
-      )}
-      {isAlbumsClicked && (
-        <>
-          <h2 className="pb-4 text-3xl text-textPrimary">Albums</h2>
-          <div className="grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-7">
-            {searchResults?.albums?.items.map(item => (
-              <SearchResultAlbumItem key={item.id} album={item} />
-            ))}
-          </div>
+
+          {isAllClicked && (
+            <AllSearchResults
+              albums={searchResults.albums}
+              artists={searchResults.artists}
+              tracks={searchResults.tracks}
+            />
+          )}
+
+          {isTracksClicked && (
+            <>
+              <h2 className="pb-4 text-3xl text-textPrimary">Tracks</h2>
+              {(searchResults?.tracks?.items?.length ?? 0) > 0 ? (
+                searchResults?.tracks?.items.map(item => (
+                  <SearchResultTrackItem key={item.id} track={item} />
+                ))
+              ) : (
+                <p className="text-white">No tracks found.</p>
+              )}
+            </>
+          )}
+
+          {isArtistsClicked && (
+            <>
+              <h2 className="pb-4 text-3xl text-textPrimary">Artists</h2>
+
+              {(searchResults?.artists?.items?.length ?? 0) > 0 ? (
+                <div className="grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-7">
+                  {searchResults?.artists?.items.map(item => (
+                    <SearchResultArtistItem key={item.id} artist={item} />
+                  ))}
+                </div>
+              ) : (
+                <p className="text-textPrimary">No artists found.</p>
+              )}
+            </>
+          )}
+
+          {isAlbumsClicked && (
+            <>
+              <h2 className="pb-4 text-3xl text-textPrimary">Albums</h2>
+              {(searchResults?.albums?.items?.length ?? 0) > 0 ? (
+                <div className="grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-7">
+                  {searchResults?.albums?.items.map(item => (
+                    <SearchResultAlbumItem key={item.id} album={item} />
+                  ))}
+                </div>
+              ) : (
+                <p className="text-textPrimary">No albums found.</p>
+              )}
+            </>
+          )}
         </>
       )}
     </>
