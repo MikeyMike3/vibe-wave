6318378c3d5254a6cd6ae98c1ea36bd71720775b
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-09-16 18:09:00 -0700

    Updated Styling to SearchBar
    Made it so that if the user is on the search page the search bar will have a magenta border

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useSearchContext } from '../hooks/context/useSearchContext';
 import { useFetchSearchResults } from '../hooks/apis/useFetchSearchResults';
 
@@ -8,6 +8,7 @@ import { faMagnifyingGlass } from '@awesome.me/kit-71c07605c0/icons/sharp/solid'
 
 export const SearchBar = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const { setSearchResults, query, setQuery } = useSearchContext();
   const fetchSearchResults = useFetchSearchResults();
@@ -35,7 +36,9 @@ export const SearchBar = () => {
   return (
     <div className="sticky top-0 z-[9999] flex w-full justify-center bg-black">
       <form onClick={() => navigate('/search')} className="flex items-center py-3">
-        <div className="flex items-center gap-2 rounded-full border-2 border-transparent bg-bgAccent px-6 py-2 transition duration-150 focus-within:border-magenta">
+        <div
+          className={`${location.pathname.includes('/search') && 'border-2 border-magenta border-transparent'}flex items-center gap-2 rounded-full bg-bgAccent px-6 py-2 transition duration-150`}
+        >
           <FontAwesomeIcon
             className="text-3xl text-textAccent duration-150 hover:text-textPrimary"
             icon={faMagnifyingGlass}
