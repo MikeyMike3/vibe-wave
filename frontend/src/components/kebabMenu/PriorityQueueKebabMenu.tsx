b660891ee80562158911db0fdac1254c65030313
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-09-25 17:39:12 -0700

    UX

--- frontend/src/components/kebabMenu/PriorityQueueKebabMenu.tsx
+++ frontend/src/components/kebabMenu/PriorityQueueKebabMenu.tsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsisVertical as faEllipsisVerticalSolid } from '@awesome.me/kit-71c07605c0/icons/sharp/solid';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { PlaySkipButton } from '../spotifyPlayer/PlaySkipButton';
 import { RemoveFromQueueButton } from '../spotifyPlayer/RemoveFromQueueButton';
@@ -11,8 +11,23 @@ type PriorityQueueKebabMenuProps = {
 
 export const PriorityQueueKebabMenu = ({ track }: PriorityQueueKebabMenuProps) => {
   const [isKebabMenuClicked, setIsKebabMenuClicked] = useState<boolean>(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  // Detect clicks outside of the menu
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsKebabMenuClicked(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [menuRef]);
   return (
-    <div className="relative">
+    <div className="relative" ref={menuRef}>
       <button onClick={() => setIsKebabMenuClicked(!isKebabMenuClicked)}>
         <FontAwesomeIcon
           className="rounded-full p-2 px-4 text-xl text-textPrimary duration-150 hover:bg-bgAccent"
--- frontend/src/components/kebabMenu/SearchedTrackKebabMenu.tsx
+++ frontend/src/components/kebabMenu/SearchedTrackKebabMenu.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { faEllipsisVertical as faEllipsisVerticalSolid } from '@awesome.me/kit-71c07605c0/icons/sharp/solid';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { AddToFrontOfPriorityQueueButton } from '../AddToFrontOfPriorityQueueButton';
@@ -11,8 +11,23 @@ type SearchedTrackKebabMenuProps = {
 
 export const SearchedTrackKebabMenu = ({ track }: SearchedTrackKebabMenuProps) => {
   const [isKebabMenuClicked, setIsKebabMenuClicked] = useState<boolean>(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  // Detect clicks outside of the menu
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsKebabMenuClicked(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [menuRef]);
   return (
-    <div className="relative">
+    <div className="relative" ref={menuRef}>
       <button onClick={() => setIsKebabMenuClicked(!isKebabMenuClicked)}>
         <FontAwesomeIcon
           className="rounded-full p-2 px-4 text-xl text-textPrimary duration-150 hover:bg-bgAccent"
