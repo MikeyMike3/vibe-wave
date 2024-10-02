dba14bb281940a2fc952f0abbbc2d9b4c6a9bd05
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-10-02 10:40:20 -0700

    Update useGetPlaylistItems.ts

--- frontend/src/hooks/apis/useGetPlaylistItems.ts
+++ frontend/src/hooks/apis/useGetPlaylistItems.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useHeaders } from './useHeaders';
 
 export const useGetPlaylistItems = (playlistId: string | undefined, updateState?: boolean) => {
@@ -6,26 +6,51 @@ export const useGetPlaylistItems = (playlistId: string | undefined, updateState?
   const accessToken = sessionStorage.getItem('accessToken');
   const apiHeaders = useHeaders(accessToken);
 
-  const getPlaylistItems = async () => {
-    try {
-      const response = await fetch(
-        `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
-        apiHeaders,
-      );
+  useEffect(() => {
+    const getPlaylistItems = async () => {
+      try {
+        const response = await fetch(
+          `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+          apiHeaders,
+        );
 
-      if (response.ok) {
-        const data = await response.json();
-        setPlaylistItems(data);
-        return data;
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+          let allPlaylistItems = [...data.items];
+          const playlistItemsMissing: number = data.total - data.items.length;
+          const loopsRequired: number = Math.ceil(playlistItemsMissing / data.limit);
+
+          for (let i = 1; i <= loopsRequired; i++) {
+            const nextPageResponse = await fetch(
+              `https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=${i * data.limit}`,
+              apiHeaders,
+            );
+
+            if (nextPageResponse.status === 200) {
+              // prettier-ignore
+              const nextPageData: SpotifyApi.PagingObject<SpotifyApi.PlaylistObjectSimplified> = await nextPageResponse.json();
+              allPlaylistItems = allPlaylistItems.concat(nextPageData.items);
+            } else {
+              throw new Error('Error fetching additional playlists');
+            }
+          }
+
+          setPlaylistItems({
+            ...data,
+            items: allPlaylistItems,
+          });
+          return data;
+        }
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error);
+    };
+
+    if (updateState) {
+      getPlaylistItems();
     }
-  };
+  }, [apiHeaders, playlistId, updateState]);
 
-  if (updateState) {
-    getPlaylistItems();
-  }
-
-  return { getPlaylistItems, playlistItems };
+  return { playlistItems };
 };
