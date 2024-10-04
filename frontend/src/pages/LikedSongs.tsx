bf44ec9f8ac9767eebd5a687c6e40ecefa6819df
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-10-04 16:23:11 -0700

    Update LikedSongs.tsx

--- frontend/src/pages/LikedSongs.tsx
+++ frontend/src/pages/LikedSongs.tsx
@@ -3,6 +3,7 @@ import { SearchedTrackKebabMenu } from '../components/kebabMenu/SearchedTrackKeb
 import { MainLoading } from '../components/MainLoading';
 import { PlayLikedTracksButton } from '../components/PlayLikedTracksButton';
 import { TrackInfo } from '../components/TrackInfo';
+import { getImageUrl } from '../functions/getImageUrl';
 import { useFetchSavedTracks } from '../hooks/apis/useFetchSavedTracks';
 
 export const LikedSongs = () => {
@@ -16,8 +17,18 @@ export const LikedSongs = () => {
     return <ErrorMessage />;
   }
 
+  const image = getImageUrl(savedTracks?.items[0].track.album.images);
+
   return (
     <div className="text-white">
+      <div className="flex items-end gap-6 pb-4">
+        <img className="h-72 w-72" src={image} />
+        <div>
+          <p className="text-textPrimary">Playlist</p>
+          <h1 className="pb-6 text-8xl font-bold text-textPrimary">Liked Songs</h1>
+          <p className="text-textAccent">{savedTracks?.items.length} Songs</p>
+        </div>
+      </div>
       <PlayLikedTracksButton likedTracks={savedTracks?.items} />
       {savedTracks?.items.map(item => (
         <div
