7b7016433e4485b71fddf34d7908abbbdc3f649d
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-10-04 15:21:45 -0700

    Added Data to LikedSongs

--- frontend/src/hooks/apis/useFetchSavedTracks.ts
+++ frontend/src/hooks/apis/useFetchSavedTracks.ts
@@ -11,8 +11,26 @@ export const useFetchSavedTracks = () => {
       if (!response.ok) {
         throw new Error('An error occurred when fetching saved tracks');
       }
-      const data = await response.json();
-      return data;
+      const data: SpotifyApi.UsersSavedTracksResponse = await response.json();
+      let allPlaylistItems = [...data.items];
+      const playlistItemsMissing: number = data.total - data.items.length;
+      const loopsRequired: number = Math.ceil(playlistItemsMissing / data.limit);
+
+      for (let i = 1; i <= loopsRequired; i++) {
+        const nextPageResponse = await fetch(
+          `https://api.spotify.com/v1/me/tracks?offset=${i * data.limit}`,
+          apiHeaders,
+        );
+
+        if (!nextPageResponse.ok) {
+          throw new Error('Error fetching additional saved tracks');
+        }
+
+        const nextPageData: SpotifyApi.UsersSavedTracksResponse = await nextPageResponse.json();
+        allPlaylistItems = allPlaylistItems.concat(nextPageData.items);
+      }
+
+      return { ...data, items: allPlaylistItems };
     } catch (error) {
       console.error('An error occurred when fetching saved tracks', error);
       throw error;
--- frontend/src/pages/LikedSongs.tsx
+++ frontend/src/pages/LikedSongs.tsx
@@ -1,3 +1,35 @@
+import { ErrorMessage } from '../components/ErrorMessage';
+import { SearchedTrackKebabMenu } from '../components/kebabMenu/SearchedTrackKebabMenu';
+import { MainLoading } from '../components/MainLoading';
+import { TrackInfo } from '../components/TrackInfo';
+import { useFetchSavedTracks } from '../hooks/apis/useFetchSavedTracks';
+
 export const LikedSongs = () => {
-  return <div className="text-white">LikedSongs</div>;
+  const { savedTracks, isLoading, isError } = useFetchSavedTracks();
+
+  if (isLoading) {
+    return <MainLoading />;
+  }
+
+  if (isError) {
+    return <ErrorMessage />;
+  }
+
+  return (
+    <div className="text-white">
+      {savedTracks?.items.map(item => (
+        <div
+          key={item.track?.id}
+          className="flex w-full items-center justify-between py-2 pl-2 hover:bg-bgAccent"
+        >
+          <TrackInfo
+            images={item.track.album.images}
+            name={item.track.name}
+            artists={item.track.artists}
+          />
+          <SearchedTrackKebabMenu track={item.track} />
+        </div>
+      ))}
+    </div>
+  );
 };
