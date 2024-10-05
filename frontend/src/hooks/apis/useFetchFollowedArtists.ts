16a49e6dbbecd2d7cf16d90f844d65a230951c6b
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-10-05 12:31:55 -0700

    Implemented useFetchFollowedArtists.ts

--- frontend/src/hooks/apis/useFetchFollowedArtists.ts
+++ frontend/src/hooks/apis/useFetchFollowedArtists.ts
@@ -6,18 +6,47 @@ export const useFetchFollowedArtists = () => {
   const apiHeader = useHeaders(accessToken);
   const fetchFollowedArtists = async () => {
     try {
-      const response = await fetch('https://api.spotify.com/v1/me/following', apiHeader);
+      const response = await fetch(
+        'https://api.spotify.com/v1/me/following?type=artist',
+        apiHeader,
+      );
 
       if (!response.ok) {
         throw new Error('Error fetching followed artists');
       }
+
       //prettier-ignore
-      const data:SpotifyApi.CursorBasedPagingObject<SpotifyApi.ArtistObjectFull> = await response.json();
-      return data;
+      const data: { artists: SpotifyApi.CursorBasedPagingObject<SpotifyApi.ArtistObjectFull> } = await response.json();
+
+      let allFollowedArtists = [...data.artists.items];
+      let nextPageUrl = data.artists.next;
+
+      while (nextPageUrl) {
+        try {
+          const nextPageResponse = await fetch(nextPageUrl, apiHeader);
+
+          if (!nextPageResponse.ok) {
+            throw new Error('Error fetching additional followed artists');
+          }
+
+          //prettier-ignore
+          const nextPageData: { artists: SpotifyApi.CursorBasedPagingObject<SpotifyApi.ArtistObjectFull> } = await nextPageResponse.json();
+
+          allFollowedArtists = allFollowedArtists.concat(nextPageData.artists.items);
+          nextPageUrl = nextPageData.artists.next;
+        } catch (error) {
+          console.error('Error fetching additional artists:', error);
+          throw error;
+        }
+      }
+
+      return { ...data, artists: { ...data.artists, items: allFollowedArtists } };
     } catch (error) {
+      console.log('Error fetching followed artists:', error);
       throw error;
     }
   };
+
   const {
     data: followedArtists,
     isLoading,
--- frontend/src/pages/FollowedArtists.tsx
+++ frontend/src/pages/FollowedArtists.tsx
@@ -13,10 +13,14 @@ export const FollowedArtists = () => {
   if (isError) {
     return <ErrorMessage />;
   }
-
   return (
     <div className="text-white">
-      {followedArtists?.items.map(item => <SearchResultArtistItem artist={item} />)}
+      {followedArtists?.artists.items.length === 0 && <p>You aren't following any artists.</p>}
+      <div className="grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-7">
+        {followedArtists?.artists.items.map(item => (
+          <SearchResultArtistItem key={item.id} artist={item} />
+        ))}
+      </div>
     </div>
   );
 };
