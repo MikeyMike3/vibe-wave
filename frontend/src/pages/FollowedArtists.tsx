ce699b56c0dd53bbdf0a2f1448c5a5e1d90cb49e
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-10-05 11:57:42 -0700

    Update FollowedArtists.tsx

--- frontend/src/pages/FollowedArtists.tsx
+++ frontend/src/pages/FollowedArtists.tsx
@@ -1,3 +1,22 @@
+import { ErrorMessage } from '../components/ErrorMessage';
+import { MainLoading } from '../components/MainLoading';
+import { SearchResultArtistItem } from '../components/SearchResultArtistItem';
+import { useFetchFollowedArtists } from '../hooks/apis/useFetchFollowedArtists';
+
 export const FollowedArtists = () => {
-  return <div className="text-white">FollowedArtists</div>;
+  const { followedArtists, isError, isLoading } = useFetchFollowedArtists();
+
+  if (isLoading) {
+    return <MainLoading />;
+  }
+
+  if (isError) {
+    return <ErrorMessage />;
+  }
+
+  return (
+    <div className="text-white">
+      {followedArtists?.items.map(item => <SearchResultArtistItem artist={item} />)}
+    </div>
+  );
 };
