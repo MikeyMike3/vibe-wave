1300a2c9d0d03b1d93e0a5ff5dfec8894c383f6b
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-10-06 11:43:16 -0700

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,18 @@
+import { ErrorMessage } from '../components/ErrorMessage';
+import { MainLoading } from '../components/MainLoading';
 import { useFetchHomePageData } from '../hooks/apis/useFetchHomePageData';
 
 export const Home = () => {
   const { homePageData, isLoading, isError } = useFetchHomePageData();
+
+  if (isLoading) {
+    return <MainLoading />;
+  }
+
+  if (isError) {
+    return <ErrorMessage />;
+  }
+
+  console.log(homePageData);
   return <div className="text-white">Home</div>;
 };
