522a0e051a79bdad5bcc86490810d39c14f8e537
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-10-06 10:55:04 -0700

    Create useFetchAlbum.ts

---
+++ frontend/src/hooks/apis/useFetchAlbum.ts
@@ -0,0 +1,31 @@
+import { useQuery } from '@tanstack/react-query';
+import { useHeaders } from './useHeaders';
+
+export const useFetchAlbum = (albumId: string) => {
+  const apiHeader = useHeaders();
+  const fetchAlbum = async () => {
+    try {
+      const response = await fetch(`https://api.spotify.com/v1/albums/${albumId}`, apiHeader);
+
+      if (!response.ok) {
+        throw new Error('error fetching albums');
+      }
+
+      const data: SpotifyApi.SingleAlbumResponse = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error when fetching albums', error);
+      throw error;
+    }
+  };
+  const {
+    data: album,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['album', albumId],
+    queryFn: fetchAlbum,
+  });
+
+  return { album, isError, isLoading };
+};
