b959ce52ae51c9c0242210ed187c40a483f10b43
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-10-07 16:39:09 -0700

    Update UserPlaylistItems.tsx

--- frontend/src/pages/UserPlaylistItems.tsx
+++ frontend/src/pages/UserPlaylistItems.tsx
@@ -6,6 +6,9 @@ import { MainLoading } from '../components/MainLoading';
 import { ErrorMessage } from '../components/ErrorMessage';
 import { useFetchPlaylistDetails } from '../hooks/apis/useFetchPlaylistDetails';
 import { getImageUrl } from '../functions/getImageUrl';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@awesome.me/kit-71c07605c0/icons/sharp/solid';
+import { ShuffleTracksButton } from '../components/spotifyPlayer/ShuffleTracksButton';
 
 export const UserPlaylistItems = () => {
   const { playlistId } = useParams();
@@ -25,24 +28,48 @@ export const UserPlaylistItems = () => {
   const image = getImageUrl(playlistDetails?.images);
 
   return (
-    <div className="text-white">
-      <h1>{playlistDetails?.name}</h1>
-      {<p>{playlistDetails?.owner.display_name}</p>}
-      <p>{playlistDetails?.description}</p>
-      <img src={image} alt={playlistDetails?.name} />
-      {playlistItems?.items.map(item => (
-        <div
-          key={item.track?.id}
-          className="flex w-full items-center justify-between py-2 pl-2 hover:bg-bgAccent"
-        >
-          <TrackInfo
-            images={item.track?.album.images}
-            name={item.track?.name}
-            artists={item.track?.artists}
-          />
-          <PlaylistItemKebabMenu track={item} />
+    <div className="mx-auto max-w-[98%]">
+      <div className="grid grid-cols-[1fr_300px] gap-5 pt-5 text-white">
+        <div>
+          <div className="flex flex-col gap-5 pb-5">
+            <div className="flex flex-col gap-5">
+              <h1 className="text-4xl text-textPrimary">{playlistDetails?.name}</h1>
+              <p className="text-textAccent">
+                By: <span className="text-textPrimary">Michael Morgan</span>{' '}
+                <span className="text-textPrimary">&#8226;</span> {playlistItems?.items.length}{' '}
+                songs
+                <span className="text-textPrimary"> &#8226;</span> Total Time of songs
+              </p>
+            </div>
+
+            <div className="flex gap-6">
+              <FontAwesomeIcon className="text-6xl" icon={faCirclePlay} color="aqua" />
+              <ShuffleTracksButton />
+            </div>
+          </div>
+          {playlistItems?.items.map(item => (
+            <div
+              key={item.track?.id}
+              className="flex w-full items-center justify-between py-2 pl-2 hover:bg-bgAccent"
+            >
+              <TrackInfo
+                images={item.track?.album.images}
+                name={item.track?.name}
+                artists={item.track?.artists}
+              />
+              <PlaylistItemKebabMenu track={item} />
+            </div>
+          ))}
         </div>
-      ))}
+        <div>
+          <div className="sticky top-5">
+            <img src={image} alt={playlistDetails?.name} />
+            <div className="flex">
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
