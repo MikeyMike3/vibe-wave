7828b63facb84026c10c723db8f635ea813fe9be
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-10-07 18:59:39 -0700

    Added Type Safety

--- frontend/src/hooks/apis/useFetchArtistInfoForTracks.ts
+++ frontend/src/hooks/apis/useFetchArtistInfoForTracks.ts
@@ -1,52 +1,50 @@
 import { useQuery } from '@tanstack/react-query';
 import { useHeaders } from './useHeaders';
 
-export const useFetchArtistInfoForTracks = (
+export const useFetchArtistImagesAndGenres = (
   playlistItems: SpotifyApi.PlaylistTrackObject[] | undefined,
 ) => {
   const apiHeader = useHeaders();
 
-  const fetchArtistInfo = async (artistId: string | undefined) => {
+  const fetchArtistInfo = async (artistId: string) => {
     const response = await fetch(`https://api.spotify.com/v1/artists/${artistId}`, apiHeader);
     if (!response.ok) {
       throw new Error('Error fetching artist info');
     }
     const data: SpotifyApi.ArtistObjectFull = await response.json();
-    return data;
+    return { images: data.images, genres: data.genres };
   };
 
   const fetchArtists = async () => {
-    if (!playlistItems) return {};
+    if (!playlistItems) return { artistData: {}, genres: [] };
 
-    const artistIds = playlistItems.slice(0, 3).map(item => item.track?.artists[0]?.id);
+    const artistIds = playlistItems
+      .slice(0, 3)
+      .map(item => item.track?.artists[0]?.id)
+      .filter((artistId): artistId is string => !!artistId);
 
-    if (artistIds) {
-      const fetchedArtistData = await Promise.all(
-        artistIds
-          .filter((artistId): artistId is string => !!artistId)
-          .map(async artistId => {
-            const data = await fetchArtistInfo(artistId);
-            return { [artistId]: data };
-          }),
-      );
+    const fetchedArtistData = await Promise.all(
+      artistIds.map(async (artistId: string) => {
+        const data = await fetchArtistInfo(artistId);
+        return { [artistId]: data };
+      }),
+    );
 
-      const combinedArtistInfo = fetchedArtistData.reduce((acc, artistData) => {
-        return { ...acc, ...artistData };
-      }, {});
+    const artistData = fetchedArtistData.reduce<{
+      [artistId: string]: { images: SpotifyApi.ImageObject[]; genres: string[] };
+    }>((acc, artist) => ({ ...acc, ...artist }), {});
 
-      return combinedArtistInfo;
-    }
+    const genres = fetchedArtistData.reduce<string[]>((acc, artist) => {
+      const artistGenres = Object.values(artist)[0].genres;
+      return [...acc, ...artistGenres];
+    }, []);
+
+    return { artistData, genres };
   };
 
-  const {
-    data: artistInfo,
-    error,
-    isLoading,
-  } = useQuery({
-    queryKey: ['artistInfo', playlistItems],
+  return useQuery({
+    queryKey: ['artistImagesGenres', playlistItems],
     queryFn: fetchArtists,
     enabled: !!playlistItems,
   });
-
-  return { artistInfo, error, isLoading };
 };
--- frontend/src/pages/UserPlaylistItems.tsx
+++ frontend/src/pages/UserPlaylistItems.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useGetPlaylistItems } from '../hooks/apis/useGetPlaylistItems';
 import { TrackInfo } from '../components/TrackInfo';
 import { PlaylistItemKebabMenu } from '../components/kebabMenu/PlaylistItemKebabMenu';
@@ -9,6 +9,7 @@ import { getImageUrl } from '../functions/getImageUrl';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay } from '@awesome.me/kit-71c07605c0/icons/sharp/solid';
 import { ShuffleTracksButton } from '../components/spotifyPlayer/ShuffleTracksButton';
+import { useFetchArtistImagesAndGenres } from '../hooks/apis/useFetchArtistInfoForTracks';
 
 export const UserPlaylistItems = () => {
   const { playlistId } = useParams();
@@ -16,6 +17,9 @@ export const UserPlaylistItems = () => {
   // I need to make another hook that combines these together so that I can Promise.All these together
   const { playlistItems, isLoading, isError } = useGetPlaylistItems(playlistId);
   const { playlistDetails } = useFetchPlaylistDetails(playlistId);
+  const { data: artistInfo } = useFetchArtistImagesAndGenres(playlistItems?.items);
+
+  console.log(artistInfo);
 
   if (isLoading) {
     return <MainLoading />;
@@ -28,51 +32,87 @@ export const UserPlaylistItems = () => {
   const image = getImageUrl(playlistDetails?.images);
 
   return (
-    <div className="mx-auto max-w-[98%]">
-      <div className="grid grid-cols-[1fr_300px] gap-5 pt-5 text-white">
-        <div>
-          <div className="flex flex-col gap-5 pb-5">
-            <div className="flex flex-col gap-5">
-              <h1 className="text-4xl text-textPrimary">{playlistDetails?.name}</h1>
-              <p className="text-textAccent">
-                By: <span className="text-textPrimary">Michael Morgan</span>{' '}
-                <span className="text-textPrimary">&#8226;</span> {playlistItems?.items.length}{' '}
-                songs
-                <span className="text-textPrimary"> &#8226;</span> Total Time of songs
-              </p>
-            </div>
-
-            <div className="flex gap-6">
-              <FontAwesomeIcon className="text-6xl" icon={faCirclePlay} color="aqua" />
-              <ShuffleTracksButton />
-            </div>
-          </div>
-          {playlistItems?.items.map(item => {
-            return (
-              <div
-                key={item.track?.id}
-                className="flex w-full items-center justify-between py-2 pl-2 hover:bg-bgAccent"
-              >
-                <TrackInfo
-                  images={item.track?.album.images}
-                  name={item.track?.name}
-                  artists={item.track?.artists}
-                />
-                <PlaylistItemKebabMenu track={item} />
+    <div
+      style={{
+        background: 'linear-gradient(0deg, rgba(0,0,0,1) 10%, rgba(28, 37, 82,1) 100%)',
+        backgroundAttachment: 'fixed', // Fixes the background when scrolling
+        backgroundSize: 'cover', // Ensures the background covers the entire element
+      }}
+    >
+      <div className="mx-auto max-w-[98%]">
+        <div className="grid grid-cols-[1fr_300px] gap-5 pt-5 text-white">
+          <div>
+            <div className="flex flex-col gap-5 pb-5">
+              <div className="flex flex-col gap-5">
+                <h1 className="text-5xl font-semibold text-textPrimary">{playlistDetails?.name}</h1>
+                <p className="text-textAccent">
+                  By: <span className="text-textPrimary">Michael Morgan</span>{' '}
+                  <span className="text-textPrimary">&#8226;</span> {playlistItems?.items.length}{' '}
+                  songs
+                  <span className="text-textPrimary"> &#8226;</span> Total Time of songs
+                </p>
               </div>
-            );
-          })}
-        </div>
-        <div>
-          <div className="sticky top-5">
-            <img src={image} alt={playlistDetails?.name} />
-            <div className="flex flex-col gap-4 pt-4">
-              {playlistItems?.items.slice(0, 3).map(item => (
-                <div className="flex items-center gap-4">
-                  <img className="h-14 w-14 rounded-full" src={item.track?.album.images[0].url} />
-                  <p>{item.track?.artists[0].name}</p>
+
+              <div className="flex gap-6">
+                <FontAwesomeIcon className="text-6xl" icon={faCirclePlay} color="aqua" />
+                <ShuffleTracksButton />
+              </div>
+            </div>
+            {playlistItems?.items.map(item => {
+              return (
+                <div
+                  key={item.track?.id}
+                  className="flex w-full items-center justify-between py-2 pl-2 hover:bg-bgAccent"
+                >
+                  <TrackInfo
+                    images={item.track?.album.images}
+                    name={item.track?.name}
+                    artists={item.track?.artists}
+                  />
+                  <PlaylistItemKebabMenu track={item} />
                 </div>
-              ))}
+              );
+            })}
+          </div>
+          <div className="sticky top-5 overflow-y-auto" style={{ height: 'calc(100vh - 225px)' }}>
+            <div>
+              <img className="h-80 object-cover pb-5" src={image} alt={playlistDetails?.name} />
+
+              <div className="flex flex-wrap gap-3">
+                {artistInfo?.genres
+                  .slice(0, 3)
+                  .map(item => (
+                    <p className="rounded-3xl border-2 border-textAccent p-3">{item}</p>
+                  ))}
+              </div>
+
+              <div className="flex flex-col gap-4 pt-4">
+                {playlistItems?.items.slice(0, 3).map(item => {
+                  const artistId = item.track?.artists[0]?.id;
+                  if (!artistId) {
+                    return;
+                  }
+                  const artist = artistInfo ? artistInfo.artistData[artistId] : null;
+
+                  return (
+                    <div key={item.track?.id} className="flex items-center gap-4">
+                      <img
+                        className="h-16 w-16 rounded-full"
+                        src={artist?.images[0]?.url}
+                        alt={item.track?.artists[0]?.name}
+                      />
+                      <div>
+                        <Link
+                          to={`/artist/${item.track?.artists[0].id}`}
+                          className="text-textPrimary hover:underline"
+                        >
+                          {item.track?.artists[0]?.name}
+                        </Link>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
