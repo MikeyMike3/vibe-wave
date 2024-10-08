f50068f8dbb8db4290c840f4b4a987cdc411c942
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-10-07 19:14:49 -0700

    Create formatTimeInHours.ts

---
+++ frontend/src/functions/formatTimeInHours.ts
@@ -0,0 +1,17 @@
+export const formatTimeInHours = (
+  playlistItems: SpotifyApi.PlaylistTrackObject[] | undefined,
+): string | undefined => {
+  if (!playlistItems) {
+    return;
+  }
+
+  const totalDurationMs = playlistItems.reduce(
+    (acc, item) => acc + (item.track?.duration_ms || 0),
+    0,
+  );
+
+  const totalHours = Math.floor(totalDurationMs / (1000 * 60 * 60));
+  const totalMinutes = Math.floor((totalDurationMs % (1000 * 60 * 60)) / (1000 * 60));
+
+  return `${totalHours}h ${totalMinutes}m`;
+};
