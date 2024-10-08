6b6b921273022cea4db9c9663b7fd365b9503efd
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-10-07 19:09:40 -0700

    Update UserPlaylistItems.tsx

--- frontend/src/pages/UserPlaylistItems.tsx
+++ frontend/src/pages/UserPlaylistItems.tsx
@@ -11,6 +11,7 @@ import { faCirclePlay } from '@awesome.me/kit-71c07605c0/icons/sharp/solid';
 import { ShuffleTracksButton } from '../components/spotifyPlayer/ShuffleTracksButton';
 import { useFetchArtistImagesAndGenres } from '../hooks/apis/useFetchArtistInfoForTracks';
 import { capitalizeFirstLetterOfEachWord } from '../functions/capitalizeFirstLetterOfEachWord';
+import { Wrapper } from '../components/styledComponents/Wrapper';
 
 export const UserPlaylistItems = () => {
   const { playlistId } = useParams();
@@ -38,85 +39,87 @@ export const UserPlaylistItems = () => {
         backgroundSize: 'cover', // Ensures the background covers the entire element
       }}
     >
-      <div className="grid grid-cols-[1fr_300px] gap-5 pt-5 text-white">
-        <div>
-          <div className="flex flex-col gap-5 pb-5">
-            <div className="flex flex-col gap-5">
-              <h1 className="text-5xl font-semibold text-textPrimary">{playlistDetails?.name}</h1>
-              <p className="text-textAccent">
-                By: <span className="text-textPrimary">Michael Morgan</span>{' '}
-                <span className="text-textPrimary">&#8226;</span> {playlistItems?.items.length}{' '}
-                songs
-                <span className="text-textPrimary"> &#8226;</span> Total Time of songs
-              </p>
-            </div>
-
-            <div className="flex gap-6">
-              <FontAwesomeIcon className="text-6xl" icon={faCirclePlay} color="aqua" />
-              <ShuffleTracksButton />
-            </div>
-          </div>
-          {playlistItems?.items.map(item => {
-            return (
-              <div
-                key={item.track?.id}
-                className="flex w-full items-center justify-between py-2 pl-2 hover:bg-bgAccent"
-              >
-                <TrackInfo
-                  images={item.track?.album.images}
-                  name={item.track?.name}
-                  artists={item.track?.artists}
-                />
-                <PlaylistItemKebabMenu track={item} />
-              </div>
-            );
-          })}
-        </div>
-        <div className="sticky top-5 overflow-y-auto" style={{ height: 'calc(100vh - 225px)' }}>
+      <Wrapper>
+        <div className="grid grid-cols-[1fr_300px] gap-5 pt-5 text-white">
           <div>
-            <img className="h-80 object-cover pb-5" src={image} alt={playlistDetails?.name} />
-
-            <div className="flex flex-wrap gap-3">
-              {artistInfo?.genres.slice(0, 3).map(item => (
-                <p
-                  key={item}
-                  className="rounded-3xl border-2 border-textPrimary p-3 px-5 text-textPrimary"
-                >
-                  {capitalizeFirstLetterOfEachWord(item)}
+            <div className="flex flex-col gap-5 pb-5">
+              <div className="flex flex-col gap-5">
+                <h1 className="text-5xl font-semibold text-textPrimary">{playlistDetails?.name}</h1>
+                <p className="text-textAccent">
+                  By: <span className="text-textPrimary">Michael Morgan</span>{' '}
+                  <span className="text-textPrimary">&#8226;</span> {playlistItems?.items.length}{' '}
+                  songs
+                  <span className="text-textPrimary"> &#8226;</span> Total Time of songs
                 </p>
-              ))}
+              </div>
+
+              <div className="flex gap-6">
+                <FontAwesomeIcon className="text-6xl" icon={faCirclePlay} color="aqua" />
+                <ShuffleTracksButton />
+              </div>
             </div>
+            {playlistItems?.items.map(item => {
+              return (
+                <div
+                  key={item.track?.id}
+                  className="flex w-full items-center justify-between py-2 pl-2 hover:bg-bgAccent"
+                >
+                  <TrackInfo
+                    images={item.track?.album.images}
+                    name={item.track?.name}
+                    artists={item.track?.artists}
+                  />
+                  <PlaylistItemKebabMenu track={item} />
+                </div>
+              );
+            })}
+          </div>
+          <div className="sticky top-5 overflow-y-auto" style={{ height: 'calc(100vh - 225px)' }}>
+            <div>
+              <img className="h-80 object-cover pb-5" src={image} alt={playlistDetails?.name} />
 
-            <div className="flex flex-col gap-4 pt-4">
-              {playlistItems?.items.slice(0, 3).map(item => {
-                const artistId = item.track?.artists[0]?.id;
-                if (!artistId) {
-                  return;
-                }
-                const artist = artistInfo ? artistInfo.artistData[artistId] : null;
+              <div className="flex flex-wrap gap-3">
+                {artistInfo?.genres.slice(0, 3).map(item => (
+                  <p
+                    key={item}
+                    className="rounded-3xl border-2 border-textPrimary p-3 px-5 text-textPrimary"
+                  >
+                    {capitalizeFirstLetterOfEachWord(item)}
+                  </p>
+                ))}
+              </div>
 
-                return (
-                  <div key={item.track?.id} className="flex items-center gap-4">
-                    <img
-                      className="h-16 w-16 rounded-full"
-                      src={artist?.images[0]?.url}
-                      alt={item.track?.artists[0]?.name}
-                    />
-                    <div>
-                      <Link
-                        to={`/artist/${item.track?.artists[0].id}`}
-                        className="text-textPrimary hover:underline"
-                      >
-                        {item.track?.artists[0]?.name}
-                      </Link>
+              <div className="flex flex-col gap-4 pt-4">
+                {playlistItems?.items.slice(0, 3).map(item => {
+                  const artistId = item.track?.artists[0]?.id;
+                  if (!artistId) {
+                    return;
+                  }
+                  const artist = artistInfo ? artistInfo.artistData[artistId] : null;
+
+                  return (
+                    <div key={item.track?.id} className="flex items-center gap-4">
+                      <img
+                        className="h-16 w-16 rounded-full"
+                        src={artist?.images[0]?.url}
+                        alt={item.track?.artists[0]?.name}
+                      />
+                      <div>
+                        <Link
+                          to={`/artist/${item.track?.artists[0].id}`}
+                          className="text-textPrimary hover:underline"
+                        >
+                          {item.track?.artists[0]?.name}
+                        </Link>
+                      </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </Wrapper>
     </div>
   );
 };
