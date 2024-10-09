85906663fe9f4d3016bed12243f4626c296fe278
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-10-08 18:01:55 -0700

    Update UserPlaylistItems.tsx

--- frontend/src/pages/UserPlaylistItems.tsx
+++ frontend/src/pages/UserPlaylistItems.tsx
@@ -37,8 +37,28 @@ export const UserPlaylistItems = () => {
       fac
         .getColorAsync(imgUrl)
         .then(color => {
-          console.log(color);
-          setBackgroundColor(color.rgba);
+          const rgbaString = color.rgba;
+
+          const rgbaArray = rgbaString.match(/[\d.]+/g);
+
+          if (rgbaArray) {
+            const rgbaValues = rgbaArray.map(Number);
+
+            const darkenFactor = 0.8;
+
+            const darkerColor = [
+              Math.floor(rgbaValues[0] * darkenFactor),
+              Math.floor(rgbaValues[1] * darkenFactor),
+              Math.floor(rgbaValues[2] * darkenFactor),
+              rgbaValues[3],
+            ];
+
+            const darkerColorString = `rgba(${darkerColor.join(',')})`;
+
+            setBackgroundColor(darkerColorString);
+          } else {
+            console.error('Invalid rgba string:', rgbaString);
+          }
         })
         .catch(error => console.error('Error extracting color:', error));
     }
