750b5a125c3863c6baf9957798d9e8e0acd16e40
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-10-09 16:42:26 -0700

    Update Playlists.tsx

--- frontend/src/pages/Playlists.tsx
+++ frontend/src/pages/Playlists.tsx
@@ -1,6 +1,7 @@
 import { ErrorMessage } from '../components/ErrorMessage';
 import { MainLoading } from '../components/MainLoading';
 import { GridContainer } from '../components/styledComponents/GridContainer';
+import { Wrapper } from '../components/styledComponents/Wrapper';
 import { UserPlaylist } from '../components/UserPlaylist';
 import { useFetchUserPlaylists } from '../hooks/apis/useFetchUserPlaylists';
 
@@ -16,17 +17,19 @@ export const Playlists = () => {
   }
 
   return (
-    <GridContainer>
-      {userPlaylists?.items.map(item => (
-        <UserPlaylist
-          key={item.id}
-          name={item.name}
-          images={item.images}
-          type={item.type}
-          owner={item.owner.display_name}
-          playlistId={item.id}
-        />
-      ))}
-    </GridContainer>
+    <Wrapper>
+      <GridContainer>
+        {userPlaylists?.items.map(item => (
+          <UserPlaylist
+            key={item.id}
+            name={item.name}
+            images={item.images}
+            type={item.type}
+            owner={item.owner.display_name}
+            playlistId={item.id}
+          />
+        ))}
+      </GridContainer>
+    </Wrapper>
   );
 };
