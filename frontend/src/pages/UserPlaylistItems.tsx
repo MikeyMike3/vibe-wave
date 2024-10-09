1074b3f1070b31cd597c9454069893af7f4e7346
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-10-09 16:32:07 -0700

    Update UserPlaylistItems.tsx

--- frontend/src/pages/UserPlaylistItems.tsx
+++ frontend/src/pages/UserPlaylistItems.tsx
@@ -126,7 +126,7 @@ export const UserPlaylistItems = () => {
                 {artistInfo?.genres.slice(0, 3).map(item => (
                   <p
                     key={item}
-                    className="rounded-3xl border-2 border-textPrimary p-3 px-5 text-textPrimary"
+                    className="rounded-3xl border-2 border-textPrimary px-4 py-2 text-textPrimary"
                   >
                     {capitalizeFirstLetterOfEachWord(item)}
                   </p>
