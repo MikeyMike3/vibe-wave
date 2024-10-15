21cb082016bd932633db997b2c60ddfa526a5fa4
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-10-14 18:14:01 -0700

    Update FollowedArtists.tsx

--- frontend/src/pages/FollowedArtists.tsx
+++ frontend/src/pages/FollowedArtists.tsx
@@ -2,6 +2,7 @@ import { ErrorMessage } from '../components/ErrorMessage';
 import { MainLoading } from '../components/MainLoading';
 import { SearchResultArtistItem } from '../components/SearchResultArtistItem';
 import { GridContainer } from '../components/styledComponents/GridContainer';
+import { Wrapper } from '../components/styledComponents/Wrapper';
 import { useFetchFollowedArtists } from '../hooks/apis/useFetchFollowedArtists';
 
 export const FollowedArtists = () => {
@@ -16,12 +17,14 @@ export const FollowedArtists = () => {
   }
   return (
     <div className="text-white">
-      {followedArtists?.artists.items.length === 0 && <p>You aren't following any artists.</p>}
-      <GridContainer>
-        {followedArtists?.artists.items.map(item => (
-          <SearchResultArtistItem key={item.id} artist={item} />
-        ))}
-      </GridContainer>
+      <Wrapper>
+        {followedArtists?.artists.items.length === 0 && <p>You aren't following any artists.</p>}
+        <GridContainer>
+          {followedArtists?.artists.items.map(item => (
+            <SearchResultArtistItem key={item.id} artist={item} />
+          ))}
+        </GridContainer>
+      </Wrapper>
     </div>
   );
 };
