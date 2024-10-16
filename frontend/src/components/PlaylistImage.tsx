cbaf29a1b016aa9521f8bd3121e035ce1e6773bc
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-10-16 15:39:08 -0700

    Update PlaylistImage.tsx

--- frontend/src/components/PlaylistImage.tsx
+++ frontend/src/components/PlaylistImage.tsx
@@ -1,17 +1,24 @@
 import { getImageUrl } from '../functions/getImageUrl';
 
 type PlaylistImageProps = {
-  images:
+  images?:
     | {
         url: string;
         height: number | null;
         width: number | null;
       }[]
     | undefined;
+  imageUrl?: string;
   alt: string | undefined;
 };
 
-export const PlaylistImage = ({ images, alt }: PlaylistImageProps) => {
-  const image = getImageUrl(images);
+export const PlaylistImage = ({ images, alt, imageUrl }: PlaylistImageProps) => {
+  let image;
+  if (images) {
+    image = getImageUrl(images);
+  } else {
+    image = imageUrl;
+  }
+
   return <img className="h-80 object-cover pb-5" src={image} alt={alt} />;
 };
