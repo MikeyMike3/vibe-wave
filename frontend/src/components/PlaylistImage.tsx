8c85679475b326e5ac97d62eb63f9ef8ff1d660d
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-10-16 15:42:18 -0700

    Update PlaylistImage.tsx

--- frontend/src/components/PlaylistImage.tsx
+++ frontend/src/components/PlaylistImage.tsx
@@ -20,5 +20,5 @@ export const PlaylistImage = ({ images, alt, imageUrl }: PlaylistImageProps) => 
     image = imageUrl;
   }
 
-  return <img className="h-80 object-cover pb-5" src={image} alt={alt} />;
+  return <img className="h-80 object-cover pb-5" src={image} alt={`${alt} album cover image.`} />;
 };
