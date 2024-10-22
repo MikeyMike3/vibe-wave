4ac2e8910711c57731ea5c1c4bfb1d8d077df70e
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-10-21 17:57:28 -0700

    Added Type Safety to useShuffleTracks.ts

--- frontend/src/hooks/spotifyPlayer/useShuffleTracks.ts
+++ frontend/src/hooks/spotifyPlayer/useShuffleTracks.ts
@@ -1,4 +1,6 @@
 import { shuffleArray } from '../../functions/shuffleArray';
+import { isPlaylistTrackObjectArray } from '../../types/typeGuards/isPlaylistTrackObjectArray';
+import { isSingleAlbumResponse } from '../../types/typeGuards/isSIngleAlbumResponse';
 import { usePlaybackContext } from '../context/usePlaybackContext';
 import { useQueueContext } from '../context/useQueueContext';
 import { useIndexPlaylistQueue } from './useIndexPlaylistQueue';
@@ -22,9 +24,14 @@ export const useShuffleTracks = () => {
       setShuffleTracksState(shuffle => !shuffle);
     }
 
+    if (!playlistQueue) {
+      console.log('yoyoyoyo');
+      return;
+    }
+
     if (
-      (shuffleTracksRef.current && prevQueue?.length > 0) ||
-      (shuffleTracksRef.current && playlistQueue.length > 0)
+      (shuffleTracksRef.current && isPlaylistTrackObjectArray(prevQueue)) ||
+      (shuffleTracksRef.current && isPlaylistTrackObjectArray(playlistQueue))
     ) {
       // creates deep copies of the playlistQueue
       const toBeShuffledQueue: SpotifyApi.PlaylistTrackObject[] = JSON.parse(
@@ -34,17 +41,57 @@ export const useShuffleTracks = () => {
       const shuffledQueue: SpotifyApi.PlaylistTrackObject[] = shuffleArray(toBeShuffledQueue);
       indexPlaylistQueue(0, 'set');
       setPlaylistQueue(shuffledQueue);
-      if (prevQueue.length > 0) {
-        playSongMutation({ uri: shuffledQueue[0].track?.uri, options: {} });
+      if (isPlaylistTrackObjectArray(prevQueue)) {
+        if (prevQueue.length > 0) {
+          playSongMutation({ uri: shuffledQueue[0].track?.uri, options: {} });
+        }
+      }
+    } else if (
+      (shuffleTracksRef.current && isSingleAlbumResponse(prevQueue)) ||
+      (shuffleTracksRef.current && isSingleAlbumResponse(playlistQueue))
+    ) {
+      const toBeShuffledQueue: SpotifyApi.SingleAlbumResponse = JSON.parse(
+        JSON.stringify(unShuffledQueueRef.current),
+      );
+
+      const shuffledTracks: SpotifyApi.TrackObjectFull[] = shuffleArray(
+        toBeShuffledQueue.tracks.items,
+      );
+
+      const shuffledQueue: SpotifyApi.SingleAlbumResponse = {
+        ...toBeShuffledQueue,
+        tracks: {
+          ...toBeShuffledQueue.tracks,
+          items: shuffledTracks,
+        },
+      };
+      console.log(shuffledQueue);
+      indexPlaylistQueue(0, 'set');
+      setPlaylistQueue(shuffledQueue);
+      if (isSingleAlbumResponse(prevQueue)) {
+        if (prevQueue.tracks.items.length > 0) {
+          playSongMutation({ uri: shuffledQueue.tracks.items[0].uri, options: {} });
+        }
       }
     } else if (unShuffledQueueRef.current) {
-      // Resumes the playlist from the current song when the user turns off shuffle mode.
-      const currentTrack = playerState?.track_window.current_track.name;
-      let index = unShuffledQueueRef.current.findIndex(item => item.track?.name === currentTrack);
-      index++;
-      indexPlaylistQueue(index, 'set');
+      if (isPlaylistTrackObjectArray(unShuffledQueueRef.current)) {
+        // Resumes the playlist from the current song when the user turns off shuffle mode.
+        const currentTrack = playerState?.track_window.current_track.name;
+        let index = unShuffledQueueRef.current.findIndex(item => item.track?.name === currentTrack);
+        index++;
+        indexPlaylistQueue(index, 'set');
 
-      setPlaylistQueue(unShuffledQueueRef.current);
+        setPlaylistQueue(unShuffledQueueRef.current);
+      } else if (isSingleAlbumResponse(unShuffledQueueRef.current)) {
+        const currentTrack = playerState?.track_window.current_track.name;
+        let index = unShuffledQueueRef.current.tracks.items.findIndex(
+          item => item.name === currentTrack,
+        );
+        index++;
+        indexPlaylistQueue(index, 'set');
+
+        setPlaylistQueue(unShuffledQueueRef.current);
+      }
     }
   };
   return shuffleTracks;
