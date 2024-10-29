4df3787c7b638386ea6ca93f4ffb4f7956aaf331
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-10-28 20:07:41 -0700

    Update useRemoveFromQueue.ts

--- frontend/src/hooks/spotifyPlayer/useRemoveFromQueue.ts
+++ frontend/src/hooks/spotifyPlayer/useRemoveFromQueue.ts
@@ -1,4 +1,6 @@
 import { useQueueContext } from '../../hooks/context/useQueueContext';
+import { isPlaylistTrackObjectArray } from '../../types/typeGuards/isPlaylistTrackObjectArray';
+import { isSingleAlbumResponse } from '../../types/typeGuards/isSIngleAlbumResponse';
 
 type Options = {
   shouldIndexPlaylistQueue?: boolean;
@@ -18,15 +20,45 @@ export const useRemoveFromQueue = () => {
     if (shouldIndexPriorityQueue && priorityQueue) {
       const indexToRemove = priorityQueue.findIndex(item => item.name === name);
       setPriorityQueue(prevQueue => {
-        const tempQueue = prevQueue.filter((_, index) => index !== indexToRemove);
+        const tempQueue = prevQueue?.filter((_, index) => index !== indexToRemove);
         return tempQueue;
       });
-    } else if (shouldIndexPlaylistQueue) {
-      const indexToRemove = playlistQueue.findIndex(item => item.track?.name === name);
-      setPlaylistQueue(prevQueue => {
-        const tempQueue = prevQueue.filter((_, index) => index !== indexToRemove);
-        return tempQueue;
-      });
+    } else if (shouldIndexPlaylistQueue && playlistQueue) {
+      let indexToRemove: number;
+
+      if (isPlaylistTrackObjectArray(playlistQueue)) {
+        indexToRemove = playlistQueue.findIndex(item => item.track?.name === name);
+        setPlaylistQueue(prevQueue => {
+          if (!prevQueue) {
+            return;
+          }
+          if (isPlaylistTrackObjectArray(prevQueue)) {
+            const tempQueue = prevQueue?.filter((_, index) => index !== indexToRemove);
+            return tempQueue;
+          }
+        });
+      } else if (isSingleAlbumResponse(playlistQueue)) {
+        let indexToRemove: number;
+
+        if (isSingleAlbumResponse(playlistQueue)) {
+          indexToRemove = playlistQueue.tracks.items.findIndex(item => item.name === name);
+          setPlaylistQueue(prevQueue => {
+            if (!prevQueue) {
+              return;
+            }
+            if (isSingleAlbumResponse(prevQueue)) {
+              const updatedTracks = {
+                ...prevQueue.tracks,
+                items: prevQueue.tracks.items.filter((_, index) => index !== indexToRemove),
+              };
+              return {
+                ...prevQueue,
+                tracks: updatedTracks,
+              };
+            }
+          });
+        }
+      }
     }
   };
   return removeFromQueue;
