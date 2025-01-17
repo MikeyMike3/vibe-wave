242fb145216af3ffca9f549bc8940b18c8732e6d
MikeyMike3 <michaelmorgan19103@gmail.com>
2025-01-16 18:00:57 -0800

    Update DynamicImageBgColorContext.tsx

--- frontend/src/context/DynamicImageBgColorContext.tsx
+++ frontend/src/context/DynamicImageBgColorContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useEffect, useState } from 'react';
+import { createContext, ReactNode, useEffect, useRef, useState } from 'react';
 import { modifyDynamicBgColor } from '../functions/modifyDynamicBgColor';
 import { getLuminance } from '../functions/getLuminance';
 
@@ -25,56 +25,58 @@ export const DynamicImageBgColorProvider = ({ children }: DynamicImageBgColorPro
   const [dynamicImageBgColorMuted, setDynamicImageBgColorMuted] = useState('');
   const [dynamicImageBgColorLighter, setDynamicImageBgColorLighter] = useState('');
 
+  const defaultColorRef = useRef<string>('rgba(255, 255, 255, 1)');
+
   useEffect(() => {
     setDynamicImageBgColorDark(
-      modifyDynamicBgColor(dynamicImageBgColorMaster, 0.8, 0.5) ?? 'rgba(255, 255, 255, 1)',
+      modifyDynamicBgColor(dynamicImageBgColorMaster, 0.8, 0.5) ?? defaultColorRef.current,
     );
 
     // this determines how much brighter of a color is should be based on
     //  how bright or dark the dynamicImageBgColorMaster is.
     if (getLuminance(dynamicImageBgColorMaster) <= 0.1) {
       setDynamicImageBgColorLighter(
-        modifyDynamicBgColor(dynamicImageBgColorMaster, 12, 1) ?? 'rgba(255, 255, 255, 1)',
+        modifyDynamicBgColor(dynamicImageBgColorMaster, 12, 1) ?? defaultColorRef.current,
       );
     } else if (
       getLuminance(dynamicImageBgColorMaster) >= 0.1 &&
       getLuminance(dynamicImageBgColorMaster) <= 0.15
     ) {
       setDynamicImageBgColorLighter(
-        modifyDynamicBgColor(dynamicImageBgColorMaster, 5.5, 1) ?? 'rgba(255, 255, 255, 1)',
+        modifyDynamicBgColor(dynamicImageBgColorMaster, 5.5, 1) ?? defaultColorRef.current,
       );
     } else if (
       getLuminance(dynamicImageBgColorMaster) >= 0.35 &&
       getLuminance(dynamicImageBgColorMaster) < 0.4
     ) {
       setDynamicImageBgColorLighter(
-        modifyDynamicBgColor(dynamicImageBgColorMaster, 2.3, 1) ?? 'rgba(255, 255, 255, 1)',
+        modifyDynamicBgColor(dynamicImageBgColorMaster, 2.3, 1) ?? defaultColorRef.current,
       );
     } else if (getLuminance(dynamicImageBgColorMaster) <= 0.4) {
       setDynamicImageBgColorLighter(
-        modifyDynamicBgColor(dynamicImageBgColorMaster, 2.8, 1) ?? 'rgba(255, 255, 255, 1)',
+        modifyDynamicBgColor(dynamicImageBgColorMaster, 2.8, 1) ?? defaultColorRef.current,
       );
     } else if (
       getLuminance(dynamicImageBgColorMaster) >= 0.4 &&
       getLuminance(dynamicImageBgColorMaster) <= 0.5
     ) {
       setDynamicImageBgColorLighter(
-        modifyDynamicBgColor(dynamicImageBgColorMaster, 1.6, 1) ?? 'rgba(255, 255, 255, 1)',
+        modifyDynamicBgColor(dynamicImageBgColorMaster, 1.6, 1) ?? defaultColorRef.current,
       );
     } else if (
       getLuminance(dynamicImageBgColorMaster) >= 0.5 &&
       getLuminance(dynamicImageBgColorMaster) <= 0.8
     ) {
       setDynamicImageBgColorLighter(
-        modifyDynamicBgColor(dynamicImageBgColorMaster, 1.2, 1) ?? 'rgba(255, 255, 255, 1)',
+        modifyDynamicBgColor(dynamicImageBgColorMaster, 1.2, 1) ?? defaultColorRef.current,
       );
     } else if (getLuminance(dynamicImageBgColorMaster) >= 0.8) {
       setDynamicImageBgColorLighter(
-        modifyDynamicBgColor(dynamicImageBgColorMaster, 1.1, 1) ?? 'rgba(255, 255, 255, 1)',
+        modifyDynamicBgColor(dynamicImageBgColorMaster, 1.1, 1) ?? defaultColorRef.current,
       );
     } else {
       setDynamicImageBgColorLighter(
-        modifyDynamicBgColor(dynamicImageBgColorMaster, 1.7, 1) ?? 'rgba(255, 255, 255, 1)',
+        modifyDynamicBgColor(dynamicImageBgColorMaster, 1.7, 1) ?? defaultColorRef.current,
       );
     }
   }, [dynamicImageBgColorMaster]);
