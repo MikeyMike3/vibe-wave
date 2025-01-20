2c9f13649de5729df9aa7dee42451353920ce3cb
MikeyMike3 <michaelmorgan19103@gmail.com>
2025-01-19 20:04:14 -0800

    Implemented Searchbar
    I plan on making this code into a custom hook and its own component.

--- frontend/src/pages/Playlists.tsx
+++ frontend/src/pages/Playlists.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { ErrorMessage } from '../components/ErrorMessage';
 import { MainLoading } from '../components/MainLoading';
 import { GridContainer } from '../components/styledComponents/GridContainer';
@@ -7,6 +8,27 @@ import { useFetchUserPlaylists } from '../hooks/apis/useFetchUserPlaylists';
 
 export const Playlists = () => {
   const { data: userPlaylists, isLoading, isError } = useFetchUserPlaylists();
+  const [inputState, setInputState] = useState('');
+  const [filteredPlaylists, setFilteredPlaylists] = useState<
+    SpotifyApi.PlaylistObjectSimplified[] | undefined
+  >();
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputState(e.target.value);
+  };
+
+  useEffect(() => {
+    const lowerCaseInput = inputState.toLowerCase();
+    if (userPlaylists) {
+      const filtered = userPlaylists.items.filter(playlist =>
+        playlist.name.toLowerCase().includes(lowerCaseInput),
+      );
+      setFilteredPlaylists(filtered);
+      if (inputState.length < 0) {
+        setInputState('');
+      }
+    }
+  }, [inputState, userPlaylists]);
 
   if (isLoading) {
     return <MainLoading />;
@@ -18,10 +40,14 @@ export const Playlists = () => {
 
   return (
     <Wrapper>
+      <input placeholder="Search Playlists" onChange={onChange}></input>
       <GridContainer>
-        {userPlaylists?.items?.map(item => {
-          if (item) {
-            return (
+        {(filteredPlaylists && filteredPlaylists?.length > 0
+          ? filteredPlaylists
+          : userPlaylists?.items
+        )?.map(
+          item =>
+            item && (
               <UserPlaylist
                 key={item.id}
                 name={item.name}
@@ -30,9 +56,8 @@ export const Playlists = () => {
                 owner={item.owner?.display_name}
                 playlistId={item.id}
               />
-            );
-          }
-        })}
+            ),
+        )}
       </GridContainer>
     </Wrapper>
   );
