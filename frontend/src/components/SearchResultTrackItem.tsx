5f48531ad034a65cb871e51ba5c868fccec64ae7
MikeyMike3 <michaelmorgan19103@gmail.com>
2025-01-22 17:23:07 -0800

    Added playSkip Button to SearchResultItem Outside of KebabMenu

--- frontend/src/components/SearchResultTrackItem.tsx
+++ frontend/src/components/SearchResultTrackItem.tsx
@@ -1,3 +1,4 @@
+import { usePlaySkip } from '../hooks/spotifyPlayer/usePlaySkip';
 import { SearchedTrackKebabMenu } from './kebabMenu/SearchedTrackKebabMenu';
 
 import { TrackInfo } from './TrackInfo';
@@ -7,9 +8,20 @@ type SearchResultTrackItemProps = {
 };
 
 export const SearchResultTrackItem = ({ track }: SearchResultTrackItemProps) => {
+  const playSkip = usePlaySkip();
+
+  const handleClick = () => {
+    playSkip(track.name, { shouldPlaySong: true }, track);
+  };
+
   return (
     <div className="flex w-full items-center justify-between py-2 pl-2 hover:bg-bgAccent">
-      <TrackInfo images={track.album.images} name={track.name} artists={track.artists} />
+      <div>
+        <button className="text-white" onClick={handleClick}>
+          Play
+        </button>
+        <TrackInfo images={track.album.images} name={track.name} artists={track.artists} />
+      </div>
       <div className="flex gap-3 text-white">
         <SearchedTrackKebabMenu track={track} />
       </div>
