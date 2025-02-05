911479117335b9ef0a366b1028f2e60a8f9b6703
MikeyMike3 <michaelmorgan19103@gmail.com>
2025-02-04 16:13:12 -0800

    Re-designed Album Page

--- frontend/src/components/PlaylistImage.tsx
+++ frontend/src/components/PlaylistImage.tsx
@@ -15,5 +15,5 @@ type PlaylistImageProps = {
 export const PlaylistImage = ({ images, alt }: PlaylistImageProps) => {
   const image = getImageUrl(images);
 
-  return <img className="h-80 object-cover pb-5" src={image} alt={`${alt} album cover image.`} />;
+  return <img className="h-80 object-cover" src={image} alt={`${alt} album cover image.`} />;
 };
--- frontend/src/pages/Album.tsx
+++ frontend/src/pages/Album.tsx
@@ -3,10 +3,8 @@ import { useFetchAlbum } from '../hooks/apis/useFetchAlbum';
 import { MainLoading } from '../components/MainLoading';
 import { ErrorMessage } from '../components/ErrorMessage';
 import { Wrapper } from '../components/styledComponents/Wrapper';
-import { PlaylistItemsGrid } from '../components/userPlaylistPageComp/PlaylistItemsGrid';
 import { PlaylistItemsHeader } from '../components/userPlaylistPageComp/PlaylistItemsHeader';
 import { formatTimeInHours } from '../functions/formatTimeInHours';
-import { PlaylistItemsHeaderFlexContainer } from '../components/userPlaylistPageComp/PlaylistItemsHeaderFlexContainer';
 import { PlaylistTableColumnFlexContainer } from '../components/userPlaylistPageComp/PlaylistTableColumnFlexContainer';
 import { ShuffleTracksButton } from '../components/spotifyPlayer/ShuffleTracksButton';
 import { PlaylistItemsButtonsFlexContainer } from '../components/userPlaylistPageComp/PlaylistItemsButtonsFlexContainer';
@@ -52,72 +50,57 @@ export const Album = () => {
   }
 
   return (
-    <div className="relative">
-      {/* Blurred background */}
-      <div
-        className="absolute inset-0 z-[-1] blur-3xl"
-        style={{
-          backgroundImage: `url(${image})`,
-          backgroundAttachment: 'fixed',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      >
-        <div className="absolute inset-0 z-[-1] bg-black opacity-80"></div>
-      </div>
-      <Wrapper>
-        <PlaylistItemsGrid>
-          <PlaylistTableColumnFlexContainer>
-            <PlaylistItemsHeaderFlexContainer>
-              <OpenInSpotifyButton spotifyUrl={album?.external_urls.spotify} />
-              <PlaylistItemsHeader
-                playlistName={album?.name}
-                playlistOwnerName={album?.artists[0].name}
-                playlistLength={album?.tracks.total}
-                playlistTotalTime={formatTimeInHours(album?.tracks.items)}
-                artistId={album?.artists[0].id}
-                albumReleaseDate={album?.release_date}
-              />
-              <UserItemsSearchBar
-                handleInputChangeFunction={handleInputOnChange}
-                placeholder="Search Tracks"
-              />
-            </PlaylistItemsHeaderFlexContainer>
+    <Wrapper>
+      <PlaylistTableColumnFlexContainer>
+        <div className="flex">
+          {/* @ts-expect-error the images below works as intended*/}
+          <PlaylistImage images={album?.images} alt={album?.name} />
+          <div className="flex flex-col justify-end gap-3 pl-4">
+            <OpenInSpotifyButton spotifyUrl={album?.external_urls.spotify} />
+            <PlaylistItemsHeader
+              playlistName={album?.name}
+              playlistOwnerName={album?.artists[0].name}
+              playlistLength={album?.tracks.total}
+              playlistTotalTime={formatTimeInHours(album?.tracks.items)}
+              artistId={album?.artists[0].id}
+              albumReleaseDate={album?.release_date}
+            />
+            <UserItemsSearchBar
+              handleInputChangeFunction={handleInputOnChange}
+              placeholder="Search Album Tracks"
+            />
             <PlaylistItemsButtonsFlexContainer>
               {/* <PlayLikedTracksButton likedTracks={album?.tracks.items} /> */}
               <PlayAlbumTracksPlayButton album={album} />
               <ShuffleTracksButton />
             </PlaylistItemsButtonsFlexContainer>
-            <PlaylistItemsTable>
-              {input.length > 0 && filteredAlbumItemsArray?.length === 0 ? (
-                <p>No tracks found.</p>
-              ) : (
-                (filteredAlbumItemsArray && filteredAlbumItemsArray.length > 0
-                  ? filteredAlbumItemsArray
-                  : album?.tracks?.items
-                )?.map((item, index) => (
-                  <AlbumItemsTR
-                    key={item.id || index} // Ensure a unique key
-                    position={index + 1}
-                    image={image}
-                    trackName={item.name}
-                    artists={item.artists}
-                    trackLength={item.duration_ms}
-                    track={item}
-                    trackId={item.id}
-                    album={album}
-                    filteredAlbumItemsArray={filteredAlbumItemsArray}
-                  />
-                ))
-              )}
-            </PlaylistItemsTable>
-          </PlaylistTableColumnFlexContainer>
-          <div className="sticky top-5 overflow-y-auto" style={{ height: 'calc(100vh - 245px)' }}>
-            {/* @ts-expect-error the images below works as intended*/}
-            <PlaylistImage images={album?.images} alt={album?.name} />
           </div>
-        </PlaylistItemsGrid>
-      </Wrapper>
-    </div>
+        </div>
+
+        <PlaylistItemsTable>
+          {input.length > 0 && filteredAlbumItemsArray?.length === 0 ? (
+            <p>No tracks found.</p>
+          ) : (
+            (filteredAlbumItemsArray && filteredAlbumItemsArray.length > 0
+              ? filteredAlbumItemsArray
+              : album?.tracks?.items
+            )?.map((item, index) => (
+              <AlbumItemsTR
+                key={item.id || index} // Ensure a unique key
+                position={index + 1}
+                image={image}
+                trackName={item.name}
+                artists={item.artists}
+                trackLength={item.duration_ms}
+                track={item}
+                trackId={item.id}
+                album={album}
+                filteredAlbumItemsArray={filteredAlbumItemsArray}
+              />
+            ))
+          )}
+        </PlaylistItemsTable>
+      </PlaylistTableColumnFlexContainer>
+    </Wrapper>
   );
 };
