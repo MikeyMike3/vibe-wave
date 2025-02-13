6ccae296a9d589d0c9e72fea691d2a044a0bf3dd
MikeyMike3 <michaelmorgan19103@gmail.com>
2025-02-12 17:17:14 -0800

    Update OpenInSpotifyButton.tsx

--- frontend/src/components/OpenInSpotifyButton.tsx
+++ frontend/src/components/OpenInSpotifyButton.tsx
@@ -9,7 +9,7 @@ export const OpenInSpotifyButton = ({ spotifyUrl }: OpenInSpotifyButtonProps) =>
     spotifyUrl && (
       <a target="_blank" href={spotifyUrl}>
         <button className="w-full text-left text-textPrimary duration-150 hover:text-aqua">
-          <div className="justify-left flex gap-2">
+          <div className="flex items-center gap-2">
             <img src={spotifyImage} className="w-6" />
             Open In Spotify
           </div>
