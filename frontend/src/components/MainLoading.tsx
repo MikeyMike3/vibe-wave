c9b485da0b6d5b086e973dd3fcaf5f7a100843d4
MikeyMike3 <michaelmorgan19103@gmail.com>
2025-02-16 12:36:57 -0800

    Added Loading Spinner

--- frontend/src/components/MainLoading.tsx
+++ frontend/src/components/MainLoading.tsx
@@ -1,3 +1,17 @@
+import { CircleLoader } from 'react-spinners';
+
 export const MainLoading = () => {
-  return <div className="text-white">Loading...</div>;
+  return (
+    <div style={{ height: 'calc(100vh - 200px)' }} className="flex items-center justify-center">
+      <div className="flex flex-col items-center gap-6">
+        <h1 className="text-4xl">
+          <span className="text-aqua">Vibe</span>
+          <span className="text-magenta">Wave</span>
+        </h1>
+        <p className="text-center text-aqua">Good things take time, this may take a moment!</p>
+
+        <CircleLoader color="aqua" />
+      </div>
+    </div>
+  );
 };
