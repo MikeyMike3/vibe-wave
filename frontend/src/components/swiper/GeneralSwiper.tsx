4a0ff6e0219e66515e7668f94d27c9a01d5660ee
MikeyMike3 <michaelmorgan19103@gmail.com>
2025-03-02 15:27:39 -0800

    Update GeneralSwiper.tsx

--- frontend/src/components/swiper/GeneralSwiper.tsx
+++ frontend/src/components/swiper/GeneralSwiper.tsx
@@ -1,7 +1,7 @@
 import { Swiper, SwiperRef } from 'swiper/react';
 
 import 'swiper/css';
-import { RefObject } from 'react';
+import { RefObject, useEffect, useState } from 'react';
 
 type GeneralSwiperProps = {
   children: React.ReactNode;
@@ -9,10 +9,28 @@ type GeneralSwiperProps = {
 };
 
 export const GeneralSwiper = ({ children, swiperRef }: GeneralSwiperProps) => {
+  const [width, setWidth] = useState(
+    typeof window !== 'undefined' && window.innerWidth < 1024
+      ? 'calc(100vw - 35px)'
+      : 'calc(100vw - 270px)', // Adjust as needed for larger screens
+  );
+
+  useEffect(() => {
+    const updateWidth = () => {
+      setWidth(window.innerWidth < 1024 ? 'calc(100vw - 35px)' : 'calc(100vw - 270px)');
+    };
+
+    // Listen for window resize events
+    window.addEventListener('resize', updateWidth);
+
+    // Cleanup event listener on unmount
+    return () => window.removeEventListener('resize', updateWidth);
+  }, []);
+
   return (
     <Swiper
       ref={swiperRef}
-      style={{ width: 'calc(100vw - 285px)' }}
+      style={{ width }}
       grabCursor={true}
       slidesPerView={'auto'}
       direction="horizontal"
