3d7af4297c9c8a2dec50c15d6386caafaa038a9b
MikeyMike3 <michaelmorgan19103@gmail.com>
2025-03-02 13:41:43 -0800

    Update GridContainer.tsx

--- frontend/src/components/styledComponents/GridContainer.tsx
+++ frontend/src/components/styledComponents/GridContainer.tsx
@@ -6,8 +6,8 @@ type GridContainerProps = {
 //prettier-ignore
 export const GridContainer: React.FC<GridContainerProps> = ({ children }) => {
   return (
-    <div className="grid h-full w-full grid-cols-[repeat(auto-fill,_minmax(min(175px,_100%),_1fr))] xl:grid-cols-[repeat(auto-fill,_minmax(min(200px,_100%),_1fr))]
- gap-3 pt-6 ">
+    <div className="grid h-full w-full sm:grid-cols-[repeat(auto-fill,_minmax(min(175px,_100%),_1fr))] xl:grid-cols-[repeat(auto-fill,_minmax(min(200px,_100%),_1fr))]
+ gap-3 pt-6 grid-cols-[repeat(auto-fill,_minmax(min(125px,_100%),_1fr))]">
       {children}
     </div>
   );
