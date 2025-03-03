7cf89aa72dcd92265abdf37dbc3eec282b2e7b5b
MikeyMike3 <michaelmorgan19103@gmail.com>
2025-03-02 18:36:55 -0800

    Update Wrapper.tsx

--- frontend/src/components/styledComponents/Wrapper.tsx
+++ frontend/src/components/styledComponents/Wrapper.tsx
@@ -6,7 +6,7 @@ type WrapperProps = {
 export const Wrapper = ({ children, shouldNotIncludePadding }: WrapperProps) => {
   return (
     <div
-      className={`${shouldNotIncludePadding ? 'mx-auto max-w-[98%]' : 'mx-auto max-w-[98%] py-5'}`}
+      className={`${shouldNotIncludePadding ? 'mx-auto max-w-[95%] lg:max-w-[98%]' : 'mx-auto max-w-[95%] py-5 lg:max-w-[98%]'}`}
     >
       {children}
     </div>
