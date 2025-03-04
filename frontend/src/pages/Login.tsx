8cea26ee73f82206f9c10aa8f862e200a500166b
MikeyMike3 <michaelmorgan19103@gmail.com>
2025-03-03 17:20:31 -0800

    Update Login.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,11 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UseAuthContext } from '../hooks/context/useAuthContext';
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const VITE_API_BASE_LOGIN_URL = import.meta.env.VITE_API_BASE_LOGIN_URL;
 
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=7e8eea6f397341778fa5b6167d418e34&response_type=code&redirect_uri=${API_BASE_URL}/login&scope=streaming%20user-read-email%20user-read-private%20playlist-read-collaborative%20user-library-read%20playlist-read-private%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-follow-read%20user-top-read`;
+console.log(VITE_API_BASE_LOGIN_URL);
+
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=7e8eea6f397341778fa5b6167d418e34&response_type=code&redirect_uri=${VITE_API_BASE_LOGIN_URL}/login&scope=streaming%20user-read-email%20user-read-private%20playlist-read-collaborative%20user-library-read%20playlist-read-private%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-follow-read%20user-top-read`;
 
 export const Login = () => {
   const { login, isUserLoggedIn, isUserPremiumMember } = UseAuthContext();
